0c2fee460a63e018375a7cc93dc2f1b2136386dc
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-18 18:50:36 +0800

    cu preload all replace

--- Checkuser-Preload.js
+++ Checkuser-Preload.js
@@ -4,7 +4,7 @@ var user = "";
 while (user === "") {
 	user = prompt("User");
 }
-wpTextbox1.innerHTML = wpTextbox1.innerHTML.replace("&lt;!-- 这里填写傀儡的操纵者 --&gt;", user);
+wpTextbox1.innerHTML = "{{sockpuppetproven|"+user+"|evidence=[[Wikipedia:用戶查核請求|Checkuser]]}}";
 wpSummary.value = "標記用戶頁，[[User:"+user+"]]的傀儡，根據[[WP:RFCU]]";
 document.all["mw-editpage-watch"].click();
 if (confirm("Save?")) wpSave.click();
