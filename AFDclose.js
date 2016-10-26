0d058bd422c4ed98a44a3bb81f6d6e9cec012faf
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-26 09:33:54 +0800

    AFDclose fix bug

--- AFDclose.js
+++ AFDclose.js
@@ -1,3 +1,6 @@
+javascript:
+(function(){
+
 if (decodeURI(location.search).match(/\?title=Wikipedia:頁面存廢討論\/記錄\/\d+\/\d+\/\d+&action=edit&section=/) !== null) {
 	var text = prompt("Result","r");
 	if (text == "" || text == null) text = "r";
@@ -9,10 +12,12 @@ if (decodeURI(location.search).match(/\?title=Wikipedia:頁面存廢討論\/記�
 	wpSummary.value = wpSummary.value+text;
 	if(confirm("Save?")) wpSave.click();
 } else if (location.search.match(/\?title=.+&action=edit/) !== null) {
-	var arr = wpTextbox1.innerHTML.match(/{{[av]fd\|.*date=(\d+)[\/-](\d+)[\/-](\d+).*}}/i);
-	wpTextbox1.innerHTML = wpTextbox1.innerHTML.replace(/{{[av]fd.+}}/i,"").replace(/&lt;noinclude&gt; *&lt;\/noinclude&gt;/i,"");
+	var arr = wpTextbox1.innerHTML.match(/{{[av]fd\|.*?date=(\d+)[\/-](\d+)[\/-](\d+).*?}}/i);
+	wpTextbox1.innerHTML = wpTextbox1.innerHTML.replace(/{{[av]fd.+?}}/i,"").replace(/&lt;noinclude&gt; *&lt;\/noinclude&gt;/i,"");
 	wpSummary.value = "關閉[[Wikipedia:頁面存廢討論/記錄/"+arr[1]+"/"+arr[2]+"/"+arr[3]+"]]";
 	if(confirm("Save?")) wpSave.click();
 } else {
 	alert("Wrong page");
 }
+
+})();
