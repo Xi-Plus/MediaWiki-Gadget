6b6d9f6b56a9dc25823df42171fbeffb5b527581
xiplus <huangxuanyuxiplus@gmail.com>
2016-10-31 12:44:30 +0800

    +sidebar

---
+++ sidebar.js
@@ -0,0 +1,4 @@
+document.getElementById("p-navigation").children[1].children[0].innerHTML += '<li><a href="/wiki/Wikipedia:頁面存廢討論">AFD</a> <a href="/wiki/Wikipedia:頁面存廢討論/積壓投票">積壓</a> <a href="/wiki/Wikipedia:頁面存廢討論/記錄">本週</a></li>';
+document.getElementById("p-navigation").children[1].children[0].innerHTML += '<li><a href="/wiki/Wikipedia:当前的破坏">VIP</a></li>';
+document.getElementById("p-navigation").children[1].children[0].innerHTML += '<li><a href="/wiki/Wikipedia:请求保护页面">PT</a></li>';
+document.getElementById("p-navigation").children[1].children[0].innerHTML += '<li><a href="/wiki/Wikipedia:用戶查核請求">RFCU</a></li>';
