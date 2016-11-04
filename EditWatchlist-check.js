dba53b04cc82c02f1b40577ef7c92172aa1bf20b
xiplus <huangxuanyuxiplus@gmail.com>
2016-11-04 17:47:22 +0800

    watchlist +IPv6

--- EditWatchlist-check.js
+++ EditWatchlist-check.js
@@ -1,8 +1,9 @@
 function User_IP() {
 	var v1 = document.all["mw-htmlform-ns2"].children[0].children[0].children[1].children;
 	for (var j = 0; j < v1.length; j++) {
-		var match = v1[j].children[0].value.match(/User:\d+\.\d+\.\d+\.\d+/);
-		if (match === null) continue;
+		var match1 = v1[j].children[0].value.match(/User:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
+		var match2 = v1[j].children[0].value.match(/User:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}\:[\dA-F]{1,4}/);
+		if (match1 === null && match2 === null) continue;
 		v1[j].children[0].click();
 	}
 }
