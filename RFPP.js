032055010916c01e462a6d608bb1f85d82840d1e
xiplus <huangxuanyuxiplus@gmail.com>
2016-11-28 20:50:37 +0800

    RFPP allow only par1

--- RFPP.js
+++ RFPP.js
@@ -6,15 +6,11 @@ var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
 var type = prompt("Type", "半");
 if (type !== "" && type !== null) {
 	var length = prompt("Length", "一週");
-	if (length !== "" && length !== null) {
-		var admin = prompt("Admin");
-		if (admin !== "" && admin !== null) {
-			wpTextbox1.value = myPrefix + "*{{RFPP|" + type + "|" + length + "|by=" + admin + "}}--~~~~" + mySuffix;
-			wpSummary.value += "標記[[T:RFPP]] 已被" + admin + " " + type + "保護" + length;
-			wpMinoredit.click();
-			wpSave.click();
-		}
-	}
+	var admin = prompt("Admin");
+	wpTextbox1.value = myPrefix + "*{{RFPP|" + type + (length!==""&&length!==null?"|"+length:"") + (admin!==""&&admin!==null?"|by="+admin:"") + "}}--~~~~" + mySuffix;
+	wpSummary.value += "標記{{RFPP|" + type + (length!==""&&length!==null?"|"+length:"") + (admin!==""&&admin!==null?"|by="+admin:"") + "}}";
+	wpMinoredit.click();
+	wpSave.click();
 }
 
 })();
