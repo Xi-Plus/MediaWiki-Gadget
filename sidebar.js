fce4348beda666860d2c9798d3055fe05086e43b
xiplus <huangxuanyuxiplus@gmail.com>
2016-11-28 20:50:59 +0800

    sidebar AFD +yesterday

--- sidebar.js
+++ sidebar.js
@@ -1,2 +1,19 @@
 var today = new Date();
-document.getElementById("p-navigation").children[1].children[0].innerHTML += '<li><a href="/wiki/Wikipedia:頁面存廢討論">AFD</a> <a href="/wiki/Wikipedia:頁面存廢討論/積壓投票">積壓</a> <a href="/wiki/Wikipedia:頁面存廢討論/記錄">本週</a> <a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+today.getFullYear()+'/'+(today.getMonth()+1<10?'0':'')+(today.getMonth()+1)+'/'+(today.getDate()<10?'0':'')+today.getDate()+'">今天</a></li><li><a href="/wiki/Wikipedia:当前的破坏">VIP</a> <a href="/wiki/Wikipedia:请求保护页面">PT</a> <a href="/wiki/Wikipedia:需要管理員注意的用戶名">UAA</a> <a href="/wiki/Wikipedia:用戶查核請求">RFCU</a></li>';
+var yesterday = new Date();
+yesterday.setDate(today.getUTCDate()-1);
+var tdby = new Date();
+tdby.setDate(yesterday.getUTCDate()-1);
+document.getElementById("p-navigation").children[1].children[0].innerHTML +=
+	'<li>'+
+		'<a href="/wiki/Wikipedia:頁面存廢討論">AFD</a> '+
+		'<a href="/wiki/Wikipedia:頁面存廢討論/積壓投票">積壓</a> '+
+		'<a href="/wiki/Wikipedia:頁面存廢討論/記錄">本週</a> '+
+		'<a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+tdby.getUTCFullYear()+'/'+(tdby.getUTCMonth()+1<10?'0':'')+(tdby.getUTCMonth()+1)+'/'+(tdby.getUTCDate()<10?'0':'')+tdby.getUTCDate()+'">前</a> '+
+		'<a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+yesterday.getUTCFullYear()+'/'+(yesterday.getUTCMonth()+1<10?'0':'')+(yesterday.getUTCMonth()+1)+'/'+(yesterday.getUTCDate()<10?'0':'')+yesterday.getUTCDate()+'">昨</a> '+
+		'<a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+today.getUTCFullYear()+'/'+(today.getUTCMonth()+1<10?'0':'')+(today.getUTCMonth()+1)+'/'+(today.getUTCDate()<10?'0':'')+today.getUTCDate()+'">今</a>'+
+	'</li><li>'+
+		'<a href="/wiki/Wikipedia:当前的破坏">VIP</a> '+
+		'<a href="/wiki/Wikipedia:请求保护页面">PT</a> '+
+		'<a href="/wiki/Wikipedia:需要管理員注意的用戶名">UAA</a> '+
+		'<a href="/wiki/Wikipedia:用戶查核請求">RFCU</a>'+
+	'</li>';
