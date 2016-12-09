4b13bf8c3bd27364a446210c3d7ca78fad46df1b
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-09 19:57:55 +0800

    Quick-fixlinkstyle rm link show

--- Quick-fixlinkstyle.js
+++ Quick-fixlinkstyle.js
@@ -4,6 +4,7 @@ javascript:
 for (var i = 0; document.all["ilf_"+i+"_3"] !== undefined; i++) {
 	if (document.all["ilf_"+i+"_21"].value !== "") {
 		document.all["ilf_"+i+"_2"].click();
+		document.all["ilf_"+i+"_22"].value = "";
 	} else {
 		document.all["ilf_"+i+"_3"].click();
 	}
