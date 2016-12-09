6a7d04a4e85fd4d707baa78311ce94624844f5a8
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-09 19:58:33 +0800

    Unsigned summary

--- Unsigned.js
+++ Unsigned.js
@@ -6,7 +6,7 @@ var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
 var username = prompt("Username");
 if (username !== "" && username !== null) {
 	wpTextbox1.value = myPrefix + "{{subst:unsigned|" + username + "}}" + mySuffix;
-	wpSummary.value = "掛上[[T:unsigned]] ([[User:" + username + "]])";
+	wpSummary.value = "[[T:unsigned|補簽名]] [[User:"+username+"|"+username+"]]（[[User talk:"+username+"|對話]]｜[[Special:Contributions/"+username+"|貢獻]]）";
 	wpMinoredit.click();
 	wpSave.click();
 }
