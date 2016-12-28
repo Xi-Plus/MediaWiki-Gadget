eb82348df953841960710057f70e26e9f225c77b
xiplus <huangxuanyuxiplus@gmail.com>
2016-12-28 19:57:39 +0800

    modify Quick-fixlinkstyle

--- Quick-fixlinkstyle.js
+++ Quick-fixlinkstyle.js
@@ -2,7 +2,9 @@ javascript:
 (function(){
 
 for (var i = 0; document.all["ilf_"+i+"_3"] !== undefined; i++) {
-	if (document.all["ilf_"+i+"_21"].value !== "") {
+	if (document.all["ilf_"+i+"_0"].parentElement.previousSibling.children[3].innerHTML === "外文維基百科無此條目") {
+		document.all["ilf_"+i+"_2"].click();
+	} else if (document.all["ilf_"+i+"_21"].value !== "") {
 		document.all["ilf_"+i+"_2"].click();
 		document.all["ilf_"+i+"_22"].value = "";
 	} else {
