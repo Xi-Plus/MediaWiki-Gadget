250e343ce2f818bafa0892d511b787247f463c6f
xiplus <huangxuanyuxiplus@gmail.com>
2017-04-19 19:50:59 +0800

    EditWatchlist-check add deleted page

--- EditWatchlist-check.js
+++ EditWatchlist-check.js
@@ -39,9 +39,20 @@ function Article_redirect() {
 		}
 	}
 }
+function Article_deleted() {
+	var ns = [0, 4, 6, 8, 10, 12, 14, 100, 118, 828, 2300, 2302];
+	for (var i = 0; i < ns.length; i++) {
+		var v1 = document.all["mw-htmlform-ns"+ns[i]];
+		if (v1 === undefined) continue;
+		var v2 = v1.children[0].children[0].children[1].children;
+		for (var j = 0; j < v2.length; j++) {
+			if (v2[j].children[1].children[0].classList.contains("new")) v2[j].children[0].click();
+		}
+	}
+}
 if (mw.config.get('wgPageName') === "Special:编辑监视列表") {
 	var checkdiv = document.createElement("DIV");
 	checkdiv.id = "checkbox";
 	document.getElementById("editwatchlist-ns0").parentNode.insertBefore(checkdiv, document.getElementById("editwatchlist-ns0").parentNode.childNodes[5]);
-	document.getElementById("checkbox").innerHTML='<button type="button" onclick="User_IP();">IP用戶頁</button><button type="button" onclick="User_IP_EmptyTalk();">IP空對話頁</button><button type="button" onclick="User_IP_Archive();">IP對話頁存檔</button><button type="button" onclick="Article_redirect();">重定向</button>';
+	document.getElementById("checkbox").innerHTML='<button type="button" onclick="User_IP();">IP用戶頁</button><button type="button" onclick="User_IP_EmptyTalk();">IP空對話頁</button><button type="button" onclick="User_IP_Archive();">IP對話頁存檔</button><button type="button" onclick="Article_redirect();">重定向</button><button type="button" onclick="Article_deleted();">已刪頁面</button>';
 }
