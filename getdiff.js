f005c9062fe2b70a5b776dfaa75e276e6c0caaef
xiplus <huangxuanyuxiplus@gmail.com>
2017-04-21 11:58:15 +0800

    fix getdiff
    diff=prev&oldid=xxx

--- getdiff.js
+++ getdiff.js
@@ -2,11 +2,19 @@ javascript:
 (function(){
 
 var searchs = document.location.search.substr(1).split("&");
+var diff = "";
+var oldid = "";
 for (var i = searchs.length - 1; i >= 0; i--) {
 	if (searchs[i].substr(0, 5) === "diff=") {
-		prompt("wikitext", "[[Special:diff/"+searchs[i].substr(5)+"]]");
-		break;
+		diff = searchs[i].substr(5);
+	} else if (searchs[i].substr(0, 6) === "oldid=") {
+		oldid = searchs[i].substr(6);
 	}
+}
+if (diff === "prev") {
+	prompt("wikitext", "[[Special:diff/"+oldid+"]]");
+} else {
+	prompt("wikitext", "[[Special:diff/"+diff+"]]");
 }
 
 })();
