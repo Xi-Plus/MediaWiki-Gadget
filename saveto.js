f0e5f0b5dc533dc1e7357cda741a2469e3b9fa84
xiplus <huangxuanyuxiplus@gmail.com>
2017-05-04 18:00:20 +0800

    saveto more pages

--- saveto.js
+++ saveto.js
@@ -3,8 +3,9 @@ javascript:
 
 var page = prompt("page");
 if (page == "" || page == null) return;
+pages = page.split("|");
 wpTextbox1.value = wpTextbox1.value.replace(/(==.+==)\n\n?/, "$1\n{{saveto|"+page+"}}\n");
-wpSummary.value = wpSummary.value+" saveto [["+page+"]]";
+wpSummary.value = wpSummary.value+" saveto [["+pages.join("]]、[[")+"]]";
 wpMinoredit.click();
 if(confirm("Save?")) wpSave.click();
 
