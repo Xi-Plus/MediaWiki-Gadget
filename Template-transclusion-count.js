541b359aa42b3c16de6b53a30d1c8807e61e6649
xiplus <huangxuanyuxiplus@gmail.com>
2017-05-17 20:06:47 +0800

    Template-transclusion-count add link

--- Template-transclusion-count.js
+++ Template-transclusion-count.js
@@ -1,23 +1,28 @@
 if (mw.config.get('wgNamespaceNumber') == 10) {
+	var path = mw.config.get('wgArticlePath');
+	var node = document.createElement("span");
+	node.id = "template-transclusion-count";
+	node.innerHTML = '使用量:<a id="ttclink" href="' + path.replace('$1', 'Special:链入页面/' + mw.config.get('wgPageName') + '?hidelinks=1&hideredirs=1') + '">未取得</a>';
+	document.getElementsByClassName("mw-indicators mw-body-content")[0].appendChild(node);
 	$.ajax({
 		type: 'GET',
 		url: "https://xiplus.twbbs.org/Xiplus-zhWP/Template-transclusion-count.php?title="+mw.config.get('wgTitle'),
 		success: function success(data) {
 			data = JSON.parse(data);
 			if (data.status) {
-				document.getElementsByClassName("mw-indicators mw-body-content")[0].innerHTML+="使用量:"+data.result;
+				ttclink.innerHTML = data.result;
 			} else {
 				if (data.result == "fetch") {
-					document.getElementsByClassName("mw-indicators mw-body-content")[0].innerHTML+="抓取使用量錯誤2";
+					ttclink.innerHTML = "抓取錯誤2";
 				} else if (data.result == "match") {
-					document.getElementsByClassName("mw-indicators mw-body-content")[0].innerHTML+="分析使用量錯誤";
+					ttclink.innerHTML = "分析錯誤";
 				} else {
-					document.getElementsByClassName("mw-indicators mw-body-content")[0].innerHTML+="未知錯誤";
+					ttclink.innerHTML = "未知錯誤";
 				}
 			}
 		},
 		error: function error(e) {
-			document.getElementsByClassName("mw-indicators mw-body-content")[0].innerHTML+="抓取使用量錯誤1";
+			ttclink.innerHTML = "抓取錯誤1";
 		}
 	});
 }
