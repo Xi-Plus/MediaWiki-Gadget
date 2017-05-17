c52d8233b2e529499845ca2b4d5db4c892a1c276
xiplus <huangxuanyuxiplus@gmail.com>
2017-05-17 09:31:16 +0800

    PatrollCount

---
+++ PatrollCount.js
@@ -0,0 +1,54 @@
+var patroll = mw.util.addPortletLink(
+	'p-personal',
+	'#',
+	'patrollcount',
+	'pt-mypatroll',
+	'patrollcount',
+	'',
+	'#pt-mycontris'
+);
+var path = mw.config.get('wgArticlePath');
+patroll.innerHTML =
+	'(T <a id="unpatrollTalk" href="' + path.replace('$1', 'Special:最新页面?hidepatrolled=1&namespace=1') + '">?</a>)' +
+	'(U <a id="unpatrollUser" href="' + path.replace('$1', 'Special:最新页面?hidepatrolled=1&namespace=2') + '">?</a>)' +
+	'(UT <a id="unpatrollUsertalk" href="' + path.replace('$1', 'Special:最新页面?hidepatrolled=1&namespace=3') + '">?</a>)';
+
+var api = new mw.Api();
+function getPatrollCount() {
+	api.get({
+		action: "query",
+		format: "json",
+		list: "recentchanges",
+		rcnamespace: "1",
+		rcshow: "!patrolled",
+		rclimit: "max",
+		rctype: "new"
+		}).done( function ( data ) {
+			unpatrollTalk.innerHTML = data.query.recentchanges.length;
+		});
+	api.get({
+		action: "query",
+		format: "json",
+		list: "recentchanges",
+		rcnamespace: "2",
+		rcshow: "!patrolled",
+		rclimit: "max",
+		rctype: "new"
+		}).done( function ( data ) {
+			unpatrollUser.innerHTML = data.query.recentchanges.length;
+		});
+	api.get({
+		action: "query",
+		format: "json",
+		list: "recentchanges",
+		rcnamespace: "3",
+		rcshow: "!patrolled",
+		rclimit: "max",
+		rctype: "new"
+		}).done( function ( data ) {
+			unpatrollUsertalk.innerHTML = data.query.recentchanges.length;
+		});
+	console.log("update PatrollCount");
+}
+setInterval(getPatrollCount, 60*1000);
+getPatrollCount();
