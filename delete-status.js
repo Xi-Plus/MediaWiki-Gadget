5ff7e8182e923fb0d7116b8b96ceffd371f068bf
xiplus <huangxuanyuxiplus@gmail.com>
2017-05-18 22:05:58 +0800

    delete-status add new del log comment cv

--- delete-status.js
+++ delete-status.js
@@ -73,6 +73,7 @@ if (mw.config.get('wgNamespaceNumber') !== -1 && (mw.config.get('wgAction') === 
 				} else {
 					comment = comment.replace(/\[\[WP\:CSD\#(.+?)\|.+?\]\].+/g, "$1");
 					comment = comment.replace(/存废讨论通过：\[\[(.+?)\]\] \(\[\[WP\:TW\|TW\]\]\)/g, '<a href="' + path.replace('$1', '$1#' + mw.config.get('wgPageName')) + '">存廢</a>');
+					comment = comment.replace("列入[[WP:CV|侵权验证页面]]超过七日", "侵權");
 					comment = comment.replace("侵犯版权", "侵權");
 				}
 				log.push(comment);
