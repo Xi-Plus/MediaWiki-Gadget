12598afc3ff8eaf3f283b4fb583c2d2683930ca8
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-16 18:13:35 +0800

    AFD-stats fix message

--- AFD-stats.js
+++ AFD-stats.js
@@ -17,5 +17,5 @@ if (/^Wikipedia:頁面存廢討論\/記錄\/\d+\/\d+\/\d+$/.test(mw.config.get('
 			cntdel ++;
 		}
 	}
-	toctitle.innerHTML+='<br>提刪:'+cntall+' <span style="color: #000000; font-weight: bold;">已關閉:'+cntcls+'</span> <span style="color: #ba0000;">需關閉:'+cntdel+'</span> <span style="color: #0b0080;">進行中:'+(cntall-cntcls-cntdel)+'</span>';
+	document.getElementById("toc").children[0].innerHTML+='<br>提刪:'+cntall+' <span style="color: #000000; font-weight: bold;">已關閉:'+cntcls+'</span> <span style="color: #ba0000;">需關閉:'+cntdel+'</span> <span style="color: #0b0080;">進行中:'+(cntall-cntcls-cntdel)+'</span>';
 }
