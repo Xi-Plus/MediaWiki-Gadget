9217dde2b4118078434bc75834ec2abbeba2d350
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-24 12:02:58 +0800

    AFDclose fix remove noinclude

--- AFDclose.js
+++ AFDclose.js
@@ -20,7 +20,7 @@ if (decodeURI(location.search).match(/\?title=Wikipedia:頁面存廢討論\/記�
 	if(confirm("Save?")) wpSave.click();
 } else if (location.search.match(/\?title=.+&action=edit/) !== null) {
 	var arr = wpTextbox1.value.match(/{{[ACVT]fd\|.*?date=(\d+)[\/-](\d+)[\/-](\d+).*?}}/i);
-	wpTextbox1.value = wpTextbox1.value.replace(/{{[ACVT]fd.*?}}\n?/i,"").replace(/&lt;noinclude&gt; *&lt;\/noinclude&gt;\n?/i,"");
+	wpTextbox1.value = wpTextbox1.value.replace(/{{[ACVT]fd.*?}}\n?/i,"").replace(/<noinclude> *<\/noinclude>\n?/i,"");
 	if(arr!=null) wpSummary.value = "關閉[[Wikipedia:頁面存廢討論/記錄/"+arr[1]+"/"+arr[2]+"/"+arr[3]+"]]";
 	else wpSummary.value = "關閉[[Wikipedia:頁面存廢討論]]";
 	if(confirm("Save?")) wpSave.click();
