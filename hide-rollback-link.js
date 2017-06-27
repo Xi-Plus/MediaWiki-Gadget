735c9afab9b00bfbf1dd8c989af1d65285191dee
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-27 21:41:25 +0800

    HRL add histrory Recentchangeslinked

--- hide-rollback-link.js
+++ hide-rollback-link.js
@@ -1,5 +1,16 @@
-if (mw.config.get('wgPageName') === "Special:监视列表") {
+if (["Watchlist", "Recentchangeslinked"].indexOf(mw.config.get('wgCanonicalSpecialPageName')) !== -1) {
 	for (var i = document.getElementsByClassName("mw-rollback-link").length - 1; i >= 0; i--) {
-		document.getElementsByClassName("mw-rollback-link")[i].style.display="none";
+		document.getElementsByClassName("mw-rollback-link")[i].style.display = "none";
 	}
+}
+if (mw.config.get('wgAction') === "history") {
+	for (var i = document.getElementsByClassName("mw-rollback-link").length - 1; i >= 0; i--) {
+		document.getElementsByClassName("mw-rollback-link")[i].style.display = "none";
+	}
+	var node = document.createElement("input");
+	node.id = "rollbacklinkbtn";
+	node.type = "button";
+	node.value = "顯示回退";
+	pagehistory.previousSibling.insertBefore(node, pagehistory.previousSibling.children[1]);
+	rollbacklinkbtn.addEventListener("click", usercolor);
 }
