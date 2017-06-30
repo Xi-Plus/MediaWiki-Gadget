6dfd35f661afecc16920eb015796215331196ef6
xiplus <huangxuanyuxiplus@gmail.com>
2017-06-30 12:16:44 +0800

    WatchAll add watch category

--- WatchAll.js
+++ WatchAll.js
@@ -6,24 +6,30 @@ var q = "";
 if (c === "+") q = "watch";
 else if (c === "-") q = "unwatch";
 if (q !== "") {
-	var watched = [];
-	var titles = document.getElementsByClassName("mw-contributions-title");
-	var len = titles.length;
-	for (var i = 0; i < len; i++) {
-		var title = titles[i].innerText;
-		if (!watched.includes(title)) {
-			pg.fn.modifyWatchlist(title, q);
-			watched.push(title);
+	if (mw.config.get('wgCanonicalSpecialPageName') === "Contributions") {
+		var watched = [];
+		var titles = document.getElementsByClassName("mw-contributions-title");
+		var len = titles.length;
+		for (var i = 0; i < len; i++) {
+			var title = titles[i].innerText;
+			if (!watched.includes(title)) {
+				pg.fn.modifyWatchlist(title, q);
+				watched.push(title);
+			}
 		}
-	}
-	var titles = document.getElementsByClassName("mw-newpages-pagename");
-	var len = titles.length;
-	for (var i = 0; i < len; i++) {
-		var title = titles[i].innerText;
-		if (!watched.includes(title)) {
-			pg.fn.modifyWatchlist(title, q);
-			watched.push(title);
+		var titles = document.getElementsByClassName("mw-newpages-pagename");
+		var len = titles.length;
+		for (var i = 0; i < len; i++) {
+			var title = titles[i].innerText;
+			if (!watched.includes(title)) {
+				pg.fn.modifyWatchlist(title, q);
+				watched.push(title);
+			}
 		}
+	} else if (mw.config.get('wgCanonicalNamespace') === "Category") {
+		$(".mw-category-generated a").each(function(i,e){
+			pg.fn.modifyWatchlist(e.innerText, q);
+		});
 	}
 }
 
