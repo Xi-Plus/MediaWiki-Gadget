dd0621ace6dfb01489efb68ec745e951424a8a94
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-19 17:30:00 +0800

    ping add no sign

--- ping.js
+++ ping.js
@@ -4,6 +4,10 @@ javascript:
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
 var list = myPrefix.match(/\[\[(User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#]+[^\]]*\]\]/gi);
+if (list === null) {
+	wpTextbox1.value = myPrefix + "{{ping|}}" + mySuffix;
+	return;
+}
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
 	var temp = list[i].match(/\[\[(?:User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#]+)[^\]]*\]\]/i);
