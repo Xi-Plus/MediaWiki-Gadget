d57182e6d36561cde92a671d55c26f5b713fb4a0
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-19 17:37:28 +0800

    ping ignore subpage

--- ping.js
+++ ping.js
@@ -3,14 +3,14 @@ javascript:
 
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
-var list = myPrefix.match(/\[\[(User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#]+[^\]]*\]\]/gi);
+var list = myPrefix.match(/\[\[(User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
 if (list === null) {
 	wpTextbox1.value = myPrefix + "{{ping|}}" + mySuffix;
 	return;
 }
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
-	var temp = list[i].match(/\[\[(?:User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#]+)[^\]]*\]\]/i);
+	var temp = list[i].match(/\[\[(?:User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
 	if (temp !== null && temp[1] != "A2093064") {
 		username = temp[1];
 		break;
