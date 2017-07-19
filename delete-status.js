a040ca844a3886468476d0b3a2ad87d8b44f67f3
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-19 17:29:47 +0800

    delete-status fix

--- delete-status.js
+++ delete-status.js
@@ -77,9 +77,9 @@ if (mw.config.get('wgNamespaceNumber') !== -1 && (mw.config.get('wgAction') === 
 				} else if (comment.match("被取代的非自由图像版本") !== null) {
 					continue;
 				} else {
-					comment = comment.replace(/.*\[\[:?(?:WP|Wikipedia)\:CSD\#([^|\]]+).*/g, "$1");
+					comment = comment.replace(/\[\[:?(?:WP|Wikipedia)\:CSD\#([^|\]]+)/g, "$1");
 					comment = comment.replace(/^内容为：.+/, "空");
-					comment = comment.replace(/.*?(G1|G2|G3|G5|G8|G10|G11|G12|G13|G14|G15|G16|A1|A2|A3|A5|A6|R2|R3|R5|F1|F3|F4|F5|F6|F7|O1|O3|O4).*/i, "$1");
+					comment = comment.replace(/.*?(G1|G2|G3|G5|G8|G10|G11|G12|G13|G14|G15|G16|A1|A2|A3|A5|A6|R2|R3|R5|F1|F3|F4|F5|F6|F7|O1|O3|O4)[^\d].*/i, "$1");
 					comment = comment.replace(/.*?((Wikipedia|维基百科):(頁面|檔案)存廢討論\/記錄\/\d{4}\/\d{2}\/\d{2}).*/g, '<a href="' + path.replace('$1', '$1#' + mw.config.get('wgPageName')) + '">存廢</a>');
 					comment = comment.replace(/根據投票結果刪除.*/, "存廢");
 					comment = comment.replace("列入[[WP:CV|侵权验证页面]]超过七日", "侵權");
