30c178993800712dd44c9a642cba00b92a4aa704
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-25 17:38:18 +0800

    Unsigned-last

---
+++ Unsigned-last.js
@@ -0,0 +1,54 @@
+javascript:
+(function(){
+
+var username = "";
+$.ajax({
+	type: "GET",
+	url: "https://zh.wikipedia.org/w/api.php",
+	data: {
+		action: "query",
+		format: "json",
+		prop: "revisions",
+		titles: mw.config.get('wgPageName'),
+		rvprop: "user",
+		rvlimit: "1"
+	},
+	success: function success(data) {
+		var message = "找不到提刪者";
+		var path = mw.config.get('wgArticlePath');
+		for (var id in data.query.pages) {
+			username = data.query.pages[id].revisions[0].user;
+			break;
+		}
+		if (username === null || username === "") {
+			alert("讀取編者錯誤");
+		} else {
+			next();
+		}
+	},
+	error: function error(e) {
+		alert("抓取最後編者錯誤");
+	}
+});
+
+function unsignedit(content){
+	return content + "{{subst:unsigned|" + username + "}}";
+}
+
+function noticeedit(content){
+	return content + "\n==您忘了簽名==\n{{subst:Uw-tilde|"+mw.config.get('wgPageName')+"}}--~~~~";
+}
+
+function finish(){
+	APIedit(mw.config.get('wgPageName'), "[[T:unsigned|補簽名]] [[User:"+username+"|"+username+"]]（[[User talk:"+username+"|對話]]｜[[Special:Contributions/"+username+"|貢獻]]）", unsignedit, true);
+};
+
+function next(){
+	if (confirm("Uw-tilde?"+username)) {
+		APIedit("User_talk:"+username, "單層級通知：沒有在討論頁上簽名，於[["+mw.config.get('wgPageName')+"]]", noticeedit, false, finish);
+	} else {
+		finish();
+	}
+}
+
+})();
