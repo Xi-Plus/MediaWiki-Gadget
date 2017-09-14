2ddd01957a9e5e4c369cdb0fa229b800d46b51e9
xiplus <huangxuanyuxiplus@gmail.com>
2017-09-14 18:29:23 +0800

    AFDdeletelog -> AFDpagelog

--- AFDpagelog.js
+++ AFDpagelog.js
@@ -3,8 +3,8 @@ if (/^Wikipedia:頁面存廢討論\/記錄\/\d+\/\d+\/\d+$/.test(mw.config.get('
 	for (var i = 0; i < list.length; i++) {
 		if (list[i].getAttribute("href").indexOf("/") === 0) {
 			var node = document.createElement("a");
-			node.href = "/wiki/Special:日志/delete?page="+list[i].innerText;
-			node.innerText = "(刪紀錄)";
+			node.href = "/wiki/Special:日志?page="+list[i].innerText;
+			node.innerText = "(日誌)";
 			list[i].parentElement.parentElement.appendChild(node);
 		}
 	}
