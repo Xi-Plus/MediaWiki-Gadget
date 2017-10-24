c3b2a6ed65bb05221ba77e0b711df7d8635ca286
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-24 18:27:08 +0800

    translatewiki

---
+++ translatewiki.js
@@ -0,0 +1,46 @@
+javascript:
+(function(){
+
+lang = "zh-hant";
+
+function getTranslatewikiLink(str) {
+	match = str.match(/\(([^:)]+)/g);
+	res = [];
+	if (match !== undefined) {
+		$(match).each(function(i, m){
+			m = m.substr(1);
+			res.push('<a href="https://translatewiki.net/wiki/MediaWiki:'+m+'/'+lang+'" target="_blank">('+m+')</a>');
+		});
+	}
+	return res;
+}
+$(document).click(function(event) {
+	el = $(event.target);
+	if ($(el).attr("class") == "mw-notification-content") {
+		return;
+	}
+	msg = "translatewiki links:";
+	msg += "<br>text: ";
+	hasmsg = false;
+	if (el.text() !== undefined) {
+		res = getTranslatewikiLink(el.text());
+		$(res).each(function(i, a){
+			msg += a+" ";
+			hasmsg = true;
+		});
+	}
+	msg += "<br>title: ";
+	if (el.attr("title") !== undefined) {
+		res = getTranslatewikiLink(el.attr("title"));
+		$(res).each(function(i, a){
+			msg += a+" ";
+			hasmsg = true;
+		});
+	}
+	if (hasmsg) {
+		mw.notify([msg]);
+	}
+	return false;
+});
+
+})();
