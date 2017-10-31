6910eddfd265cc670e228ab1c7b2fb88605b1510
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-31 19:36:38 +0800

    getSearchResult format result

--- getSearchResult.js
+++ getSearchResult.js
@@ -1,9 +1,16 @@
 javascript:
 (function(){
 
+var defaultadd = "$1\\n";
+var add = prompt('格式化列表：\n$1,   顯示為\nA, B,\n\n*[[$1]]\\n 顯示為\n*[[A]]\n*[[B]]', defaultadd);
+if (add === null) {
+	return;
+}
+add = add.replace(/\\n/g, "<br>");
+
 var text = "";
 $(".mw-search-result-heading").each(function(i, e) {
-	text += e.children[0].innerText+"<br>";
+	text += add.replace(/\$1/g, e.children[0].innerText);
 });
 
 var win = window.open("", "Search Result");
