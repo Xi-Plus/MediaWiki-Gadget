8fe2d71160466751c8867f643cfe89e0501b166e
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-06 21:38:19 +0800

    HRTProtectLink: +dependencies

--- HRTProtectLink.js
+++ HRTProtectLink.js
@@ -1,12 +1,14 @@
 if (mw.config.get("wgNamespaceNumber") == 10 || mw.config.get("wgNamespaceNumber") == 828) {
-	mw.util.addPortletLink(
-		'p-cactions',
-		mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=autoconfirmed&wpProtectReasonSelection=高风险模板',
-		'半保護高風險模板'
-	);
-	mw.util.addPortletLink(
-		'p-cactions',
-		mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=sysop&mwProtect-level-move=sysop&wpProtectReasonSelection=高风险模板',
-		'全保護高風險模板'
-	);
+	mw.loader.using(['mediawiki.util']).done(function(){
+		mw.util.addPortletLink(
+			'p-cactions',
+			mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=autoconfirmed&mwProtect-level-move=sysop&wpProtectReasonSelection=高风险模板',
+			'半保護高風險模板'
+		);
+		mw.util.addPortletLink(
+			'p-cactions',
+			mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=sysop&mwProtect-level-move=sysop&wpProtectReasonSelection=高风险模板',
+			'全保護高風險模板'
+		);
+	});
 }
