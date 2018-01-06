07fb5d2d43fd5b7058e9e67b262a50007263e8c4
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-06 21:36:26 +0800

    hide-rollback-link.js: show link button

--- hide-rollback-link.js
+++ hide-rollback-link.js
@@ -1,4 +1,4 @@
-if (["Watchlist", "Recentchangeslinked"].indexOf(mw.config.get('wgCanonicalSpecialPageName')) !== -1) {
+if (["Watchlist", "Recentchanges", "Recentchangeslinked"].indexOf(mw.config.get('wgCanonicalSpecialPageName')) !== -1) {
 	for (var i = document.getElementsByClassName("mw-rollback-link").length - 1; i >= 0; i--) {
 		document.getElementsByClassName("mw-rollback-link")[i].style.display = "none";
 	}
@@ -7,10 +7,15 @@ if (mw.config.get('wgAction') === "history") {
 	for (var i = document.getElementsByClassName("mw-rollback-link").length - 1; i >= 0; i--) {
 		document.getElementsByClassName("mw-rollback-link")[i].style.display = "none";
 	}
+	function showrollbacklink() {
+		for (var i = document.getElementsByClassName("mw-rollback-link").length - 1; i >= 0; i--) {
+			document.getElementsByClassName("mw-rollback-link")[i].style.display = "";
+		}
+	}
 	var node = document.createElement("input");
 	node.id = "rollbacklinkbtn";
 	node.type = "button";
 	node.value = "顯示回退";
 	pagehistory.previousSibling.insertBefore(node, pagehistory.previousSibling.children[1]);
-	rollbacklinkbtn.addEventListener("click", usercolor);
+	rollbacklinkbtn.addEventListener("click", showrollbacklink);
 }
