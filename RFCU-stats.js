f86923c4b153429f04af1ab7c04110e1226c5f4b
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-06 21:35:29 +0800

    RFCU-stats: bold text

--- RFCU-stats.js
+++ RFCU-stats.js
@@ -1,5 +1,7 @@
 if (mw.config.get('wgPageName') === 'Wikipedia:用戶查核請求') {
 	for (var i = 2; $(".tocsection-"+i)[0] !== undefined; i++) {
-		var toc = $(".tocsection-"+i)[0].children[0].children[1].style["color"] = $("h3").next().find(":nth-child(1)")[i-2].style.background;
+		var sectiontext = $(".tocsection-"+i)[0].children[0].children[1];
+		sectiontext.style["color"] = $("h3").next().find(":nth-child(1)")[i-2].style.background;
+		sectiontext.style["font-weight"] = "bold";
 	}
 }
