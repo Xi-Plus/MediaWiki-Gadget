bc7ee03d77a952d61775e1b17d45a9da39cd5b90
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-06 21:38:37 +0800

    interface-global: +prefixindex

--- interface-global.js
+++ interface-global.js
@@ -3,3 +3,18 @@ document.all["pt-betafeatures"].hidden = true;
 document.all["pt-logout"].hidden = true;
 
 /* 側欄 */
+/* mediawiki.util start */
+mw.loader.using(['mediawiki.util']).done(function () {
+
+mw.util.addPortletLink(
+	'p-tb',
+	'/wiki/Special:PrefixIndex/'+mw.config.get('wgPageName'),
+	'前綴索引',
+	't-prefixindex',
+	'',
+	'',
+	$('#t-specialpages')
+);
+
+});
+/* mediawiki.util end */
