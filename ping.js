8e2e4f7fbef31047714ebbebce0a09a6192d9db5
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-10 11:51:09 +0800

    ping: custom template, myusername

--- ping.js
+++ ping.js
@@ -1,17 +1,19 @@
 javascript:
 (function(){
 
+var template = "ping";
+var myusername = "Xiplus";
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
 var list = myPrefix.match(/\[\[(User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
 if (list === null) {
-	wpTextbox1.value = myPrefix + "{{ping|}}" + mySuffix;
+	wpTextbox1.value = myPrefix + "{{"+template+"|}}" + mySuffix;
 	return;
 }
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
 	var temp = list[i].match(/\[\[(?:User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
-	if (temp !== null && temp[1] != "A2093064") {
+	if (temp !== null && temp[1] != myusername) {
 		username = temp[1];
 		break;
 	}
@@ -22,6 +24,6 @@ if (indent === null) {
 } else {
 	indent = indent[indent.length-1][0]+indent[indent.length-1];
 }
-wpTextbox1.value = myPrefix + indent+ "{{ping|"+username+"}}" + mySuffix;
+wpTextbox1.value = myPrefix + indent+ "{{"+template+"|"+username+"}}" + mySuffix;
 
 })();
