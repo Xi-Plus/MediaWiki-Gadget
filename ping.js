575057535a91cb3a412f01cd9ec8c6592ef29ed4
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-19 18:43:33 +0800

    ping: add U/UT ns

--- ping.js
+++ ping.js
@@ -5,14 +5,14 @@ var template = "ping";
 var myusername = "Xiplus";
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
-var list = myPrefix.match(/\[\[(User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
+var list = myPrefix.match(/\[\[(U|UT|User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
 if (list === null) {
 	wpTextbox1.value = myPrefix + "{{"+template+"|}}" + mySuffix;
 	return;
 }
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
-	var temp = list[i].match(/\[\[(?:User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
+	var temp = list[i].match(/\[\[(?:U|UT|User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
 	if (temp !== null && temp[1] != myusername) {
 		username = temp[1];
 		break;
