1f99c4ce9efa5f8da3b9cc14e4e31673f90ad41c
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-31 23:49:24 +0800

    +formerly

---
+++ formerly.js
@@ -0,0 +1,17 @@
+javascript:
+(function(){
+
+var newtitle = prompt("title");
+if (newtitle === null) {return;}
+if ((m = wpTextbox1.value.match(/<s>\[\[(.+?)]]<\/s>→\[\[(.+?)]]/)) !== null) {
+	var oldtitle = m[1];
+	newtitle = m[2];
+} else {
+	var oldtitle = wpTextbox1.value.match(/== *(.+?) *==/)[1];
+}
+wpTextbox1.value = wpTextbox1.value.replace(/== *(.+?) *== *\n?/, "==[["+newtitle+"]]==\n{{formerly|[["+oldtitle+"]]}}\n");
+wpSummary.value = "/* "+newtitle+" */ 替換標題[["+oldtitle+"]]→[["+newtitle+"]]";
+wpMinoredit.click();
+if(confirm("Save?")) wpSave.click();
+
+})();
