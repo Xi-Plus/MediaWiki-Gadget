6cafd37653c07ccaff00d1c9c6fd63fa21dfb6d1
xiplus <huangxuanyuxiplus@gmail.com>
2018-02-09 12:33:52 +0800

    TTC fix title encode

--- Template-transclusion-count.js
+++ Template-transclusion-count.js
@@ -3,11 +3,11 @@ if (mw.config.get('wgNamespaceNumber') == 10 || mw.config.get('wgNamespaceNumber
 	var node = document.createElement("span");
 	node.id = "template-transclusion-count";
 	node.style = "margin-left: 5px;";
-	node.innerHTML = '<a href="' + path.replace('$1', 'Special:链入页面/' + mw.config.get('wgPageName') + '?hidelinks=1&hideredirs=1&limit=500') + '">使用量</a>：<a id="ttclink" href="https://tools.wmflabs.org/templatecount/index.php?lang=zh&namespace='+mw.config.get('wgNamespaceNumber')+'&name='+mw.config.get('wgTitle')+'">未取得</a>';
+	node.innerHTML = '<a href="' + path.replace('$1', 'Special:链入页面/' + mw.config.get('wgPageName') + '?hidelinks=1&hideredirs=1&limit=500') + '">使用量</a>：<a id="ttclink" href="https://tools.wmflabs.org/templatecount/index.php?lang=zh&namespace='+mw.config.get('wgNamespaceNumber')+'&name='+encodeURIComponent(mw.config.get('wgTitle'))+'">未取得</a>';
 	document.getElementsByClassName("mw-indicators mw-body-content")[0].appendChild(node);
 	$.ajax({
 		type: 'GET',
-		url: "https://xiplus.twbbs.org/Xiplus-zhWP/Template-transclusion-count.php?namespace="+mw.config.get('wgNamespaceNumber')+"&title="+mw.config.get('wgTitle'),
+		url: "https://sp.tnfsh.tn.edu.tw/xiplus/Xiplus-zhWP/Template-transclusion-count.php?namespace="+mw.config.get('wgNamespaceNumber')+"&title="+encodeURIComponent(mw.config.get('wgTitle')),
 		success: function success(data) {
 			data = JSON.parse(data);
 			if (data.status) {
