a639c09bc2033c6350c25b780a20e0077fb7e4e3
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-04-09 09:59:16 +0800

    update HRTProtectLink

--- HRTProtectLink.js
+++ HRTProtectLink.js
@@ -2,7 +2,7 @@ if (mw.config.get("wgNamespaceNumber") == 10 || mw.config.get("wgNamespaceNumber
 	mw.loader.using(['mediawiki.util']).done(function(){
 		mw.util.addPortletLink(
 			'p-cactions',
-			mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=autoconfirmed&mwProtect-level-move=sysop&wpProtectReasonSelection=高风险模板',
+			mw.config.get("wgServer")+mw.config.get("wgScriptPath")+'/index.php?title='+encodeURIComponent(mw.config.get('wgPageName'))+'&action=protect&mwProtect-level-edit=autoconfirmed&mwProtect-level-move=autoconfirmed&wpProtectReasonSelection=高风险模板',
 			'半保護高風險模板'
 		);
 		mw.util.addPortletLink(
