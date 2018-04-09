3491c53228110f28b9509106a6faeb08810a72e7
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-04-09 09:59:04 +0800

    update interface-global

--- interface-global.js
+++ interface-global.js
@@ -10,11 +10,6 @@ function showCentralAuth(username) {
 	if (username.match('^\\d+\\.\\d+\\.\\d+\\.\\d+$') !== null || username.match('^[0-9a-fA-F]+:[0-9a-fA-F:]+$')) {
 		mw.util.addPortletLink(
 			'p-cactions',
-			'https://tools.wmflabs.org/guc/?by=date&user='+username,
-			'全域貢獻'
-		);
-		mw.util.addPortletLink(
-			'p-cactions',
 			'https://tools.wmflabs.org/meta/stalktoy/'+username,
 			'全域封禁'
 		);
@@ -30,6 +25,11 @@ function showCentralAuth(username) {
 			'全域帳號'
 		);
 	}
+	mw.util.addPortletLink(
+		'p-cactions',
+		'https://tools.wmflabs.org/guc/?by=date&user='+username,
+		'全域貢獻'
+	);
 }
 if (mw.config.get('wgNamespaceNumber') === 2 || mw.config.get('wgNamespaceNumber') === 3) {
 	showCentralAuth(mw.config.get('wgTitle').replace(/^([^/]+).*$/, '$1'));
@@ -40,13 +40,15 @@ if (mw.config.get('wgCanonicalSpecialPageName') === 'Contributions') {
 
 /* Admin list */
 switch(mw.config.get('wgDBname')) {
+	case 'zh_classicalwiki':
+	case 'zh_yuewiki':
+	case 'zhwiki':
 	case 'zhwikibooks':
 	case 'zhwikinews':
 	case 'zhwikiquote':
 	case 'zhwikisource':
 	case 'zhwikivoyage':
 	case 'zhwiktionary':
-	case 'zh_classicalwiki':
 		mw.util.addPortletLink(
 			'p-cactions',
 			mw.config.get('wgServer')+mw.config.get('wgArticlePath').replace('$1', 'Special:Userlist')+'?group=sysop',
@@ -64,6 +66,16 @@ mw.util.addPortletLink(
 	'',
 	'',
 	$('#t-specialpages')
+);
+
+mw.util.addPortletLink(
+	'p-tb',
+	'/wiki/?curid='+mw.config.get('wgArticleId'),
+	'短網址',
+	't-shorturl',
+	'',
+	'',
+	$('#t-info')
 );
 
 /* mediawiki.util */
