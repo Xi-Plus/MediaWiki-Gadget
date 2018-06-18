e55f979024481fb63d183b60957f6266ff99d8f1
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-06-18 19:16:40 +0800

    if-zhwiki: update text

--- interface-zhwiki.js
+++ interface-zhwiki.js
@@ -118,7 +118,7 @@ if (mw.config.get("wgNamespaceNumber") == 8) {
 	document.getElementById("p-navigation").children[1].children[0].innerHTML +=
 		'<li>'+
 			'<a href="/wiki/Wikipedia:頁面存廢討論">AFD</a> '+
-			'<a href="/wiki/Wikipedia:頁面存廢討論/積壓投票">積壓</a> '+
+			'<a href="/wiki/Wikipedia:頁面存廢討論/積壓討論">積壓</a> '+
 			'<a href="/wiki/Wikipedia:頁面存廢討論/記錄">本週</a> '+
 			'<a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+tdby.getUTCFullYear()+'/'+(tdby.getUTCMonth()+1<10?'0':'')+(tdby.getUTCMonth()+1)+'/'+(tdby.getUTCDate()<10?'0':'')+tdby.getUTCDate()+'">前</a> '+
 			'<a href="/wiki/Wikipedia:頁面存廢討論/記錄/'+yesterday.getUTCFullYear()+'/'+(yesterday.getUTCMonth()+1<10?'0':'')+(yesterday.getUTCMonth()+1)+'/'+(yesterday.getUTCDate()<10?'0':'')+yesterday.getUTCDate()+'">昨</a> '+
