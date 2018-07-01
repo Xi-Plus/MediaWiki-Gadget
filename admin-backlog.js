5cfd14040ddb88ddde61ac8d2303da3a07197586
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-07-01 16:24:09 +0800

    admin-backlog: unblock renamed

--- admin-backlog.js
+++ admin-backlog.js
@@ -16,7 +16,7 @@ var path = mw.config.get('wgArticlePath');
 backlog.innerHTML =
 	'<a id="adminbacklog-csd" href="' + path.replace('$1', 'Category:快速删除候选') + '">CSD 未取得</a> ' +
 	'<a id="adminbacklog-ep" href="' + path.replace('$1', 'Category:維基百科編輯被保護頁面請求') + '">EP 未取得</a> ' +
-	'<a id="adminbacklog-ub" href="' + path.replace('$1', 'Category:封禁申诉') + '">UB 未取得</a> ';
+	'<a id="adminbacklog-ub" href="' + path.replace('$1', 'Category:封禁及禁制申诉') + '">UB 未取得</a> ';
 
 var api = new mw.Api();
 function getCSD() {
@@ -48,7 +48,7 @@ function getUB() {
 		action: "query",
 		format: "json",
 		prop: "categoryinfo",
-		titles: "Category:封禁申诉"
+		titles: "Category:封禁及禁制申诉"
 		}).done( function ( data ) {
 			$.each( data.query.pages, function( i, item ) {
 				document.all["adminbacklog-ub"].innerHTML = 'UB ' + item.categoryinfo.pages;
