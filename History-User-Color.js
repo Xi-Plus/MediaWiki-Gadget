0e9fccdcd806736e1c9566d389a1e61d8227efa6
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-07-17 20:36:43 +0800

    HUC: anonymous function & create element by jquery

--- History-User-Color.js
+++ History-User-Color.js
@@ -1,32 +1,45 @@
-if (mw.config.get('wgAction') === "history") {
-	function RandomColor() {
-		return "#"+RandomColor2()+RandomColor2()+RandomColor2();
-	}
-	function RandomColor2() {
-		return Math.floor((Math.random()*155)+100).toString(16).padStart(2, "0");
-	}
-	function usercolor() {
-		var userlistel = document.getElementsByClassName("mw-userlink");
-		var username = [];
-		for (var i = userlistel.length - 1; i >= 0; i--) {
-			if (username.indexOf(userlistel[i].children[0].innerText) === -1) {
-				username.push(userlistel[i].children[0].innerText);
-			}
-		}
-		var usertocolor = [];
-		for (var i = username.length - 1; i >= 0; i--) {
-			usertocolor[username[i]] = RandomColor();
-		}
-		for (var i = userlistel.length - 1; i >= 0; i--) {
-			userlistel[i].style.background = usertocolor[userlistel[i].children[0].innerText];
-		}
-		usercolorbtn.style.background = RandomColor();
-	}
-	var node = document.createElement("input");
-	node.id = "usercolorbtn";
-	node.type = "button";
-	node.value = "用戶上色";
-	node.style.background = RandomColor();
-	pagehistory.previousSibling.insertBefore(node, pagehistory.previousSibling.children[1]);
-	usercolorbtn.addEventListener("click", usercolor);
+(function(){
+
+if (mw.config.get('wgAction') !== "history") {
+	return;
 }
+
+function RandomColor() {
+	return "#"+RandomColor2()+RandomColor2()+RandomColor2();
+}
+
+function RandomColor2() {
+	return Math.floor((Math.random()*155)+100).toString(16).padStart(2, "0");
+}
+
+function usercolor() {
+	var userlistel = document.getElementsByClassName("mw-userlink");
+	var username = [];
+	for (var i = userlistel.length - 1; i >= 0; i--) {
+		if (username.indexOf(userlistel[i].children[0].innerText) === -1) {
+			username.push(userlistel[i].children[0].innerText);
+		}
+	}
+	var usertocolor = [];
+	for (var i = username.length - 1; i >= 0; i--) {
+		usertocolor[username[i]] = RandomColor();
+	}
+	for (var i = userlistel.length - 1; i >= 0; i--) {
+		userlistel[i].style.background = usertocolor[userlistel[i].children[0].innerText];
+	}
+	$(usercolorbtn).css('background', RandomColor());
+}
+
+var $node = $('<button />', {
+	'id': 'usercolorbtn',
+	'type': 'button',
+	'title': '將相同的使用者名稱塗上相同的顏色',
+	'text': '使用者名稱上色',
+	'css': {
+		'background': RandomColor()
+	}
+});
+$node.on('click', usercolor);
+$(".historysubmit.mw-history-compareselectedversions-button").after($node);
+
+})();
