048e646db8c52094731b995d7348e8d58de54c02
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-08-24 20:57:22 +0800

    +get-list/Special-BlockList-Target

---
+++ get-list/Special-BlockList-Target.js
@@ -0,0 +1,25 @@
+/*
+ * 取得[[Special:BlockList]]中的封禁目標，在新分頁顯示列表
+ */
+
+(function(){
+
+if (mw.config.get('wgCanonicalSpecialPageName') !== 'BlockList') {
+	return;
+}
+
+mw.loader.using(['mediawiki.util'], function(){
+	var node = mw.util.addPortletLink('p-cactions', '#', '取得封禁目標列表');
+	$(node).click(function(event){
+		event.preventDefault();
+
+		var result = "";
+		$(".TablePager_col_ipb_target>a.mw-userlink>bdi").each(function(i, el){
+			result += $(el).text()+"<br>";
+		});
+		var win = window.open("", "Result");
+		win.document.body.innerHTML = result;
+	});
+});
+
+})();
