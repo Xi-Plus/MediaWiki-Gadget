27c5b90a670ee0cc4c5890cec5994f1da56b3c9f
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-02 22:30:04 +0800

    Create one-click-edit/null-edit.js

---
+++ one-click-edit/null-edit.js
@@ -0,0 +1,20 @@
+javascript: (function() {
+
+    new mw.Api().edit(mw.config.get('wgPageName'), function(revision) {
+        return {
+            text: revision.content,
+            summary: 'Null edit',
+            minor: true
+        };
+    }).then(function(e) {
+        mw.notify('成功');
+    }, function(e) {
+        if (e == 'nocreate-missing') {
+            mw.notify('頁面不存在');
+        } else {
+            mw.notify('未知錯誤');
+        }
+    });
+
+}
+)();
