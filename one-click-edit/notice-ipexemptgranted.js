7f6113672367a2023e4380b330b357b7ca8e5f07
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-04 10:58:06 +0800

    Update one-click-edit/notice-ipexemptgranted for temp option

--- one-click-edit/notice-ipexemptgranted.js
+++ one-click-edit/notice-ipexemptgranted.js
@@ -1,11 +1,21 @@
 javascript: (function() {
 
     if (mw.config.get('wgRelevantUserName') === null) {
-        mw.notify("未能找到對應用戶");
+        mw.notify('未能找到對應用戶');
         return;
     }
 
     var usertalk = 'User talk:' + mw.config.get('wgRelevantUserName');
+    
+    if (!confirm('向 ' + usertalk + '發送')) {
+        mw.notify('已取消發送通知');
+        return;
+    }
+
+    var message = '{{subst:Ipexemptgranted|temp=1}}';
+    if (confirm('永久權限？')) {
+        message = '{{subst:Ipexemptgranted}}';
+    }
 
     new mw.Api().get({
         action: 'query',
@@ -19,7 +29,7 @@ javascript: (function() {
                 page: usertalk,
                 submodule: 'new-topic',
                 nttopic: '授予IP封禁例外權通知',
-                ntcontent: '{{subst:Ipexemptgranted|temp=1}}',
+                ntcontent: message,
                 ntformat: 'wikitext',
             }).then(function(e) {
                 mw.notify('成功發送授予IP封禁例外權通知');
@@ -28,8 +38,8 @@ javascript: (function() {
             });
         } else {
             new mw.Api().newSection(usertalk,
-                "",
-                "{{subst:Ipexemptgranted|temp=1}}"
+                '',
+                '{{subst:Ipexemptgranted|temp=1}}'
             ).then(function(e) {
                 mw.notify('成功發送授予IP封禁例外權通知');
             }, function(e) {
