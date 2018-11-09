5677338154bf163a6cf4c47fdef5896f5a3c7c79
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-09 18:12:44 +0800

    Create import-from-zhwp

---
+++ one-click-edit/import-from-zhwp.js
@@ -0,0 +1,40 @@
+javascript: (function() {
+
+    var interwikipage = mw.config.get('wgPageName');
+    ;if (interwikipage === null) {
+        mw.notify('未能抓取頁面名稱');
+        return;
+    }
+
+    var namespace = mw.config.get('wgNamespaceNumber');
+    ;if (namespace === null) {
+        mw.notify('未能抓取頁面命名空間');
+        return;
+    }
+
+    var interwikisource = 'w';
+
+    if (!confirm('要匯入 ' + interwikisource + ':' + interwikipage + ' 至本頁（ns=' + namespace + '）？')) {
+        return;
+    }
+
+    new mw.Api().postWithEditToken({
+        action: 'import',
+        interwikisource: interwikisource,
+        interwikipage: interwikipage,
+        namespace: namespace,
+        fullhistory: false,
+        templates: false,
+        assignknownusers: false,
+    }).then(function(e) {
+        if (e.import.length === 0) {
+            mw.notify('沒有匯入任何頁面');
+        } else {
+            mw.notify('成功匯入');
+        }
+    }, function(e) {
+        mw.notify('未知錯誤：' + e);
+    });
+
+}
+)();
