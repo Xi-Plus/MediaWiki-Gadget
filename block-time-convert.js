54c3b7687b189b110a53f1440ef83a720e811646
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-18 20:22:31 +0800

    Fix block-time-convert
    Match new form format.

--- block-time-convert.js
+++ block-time-convert.js
@@ -1,6 +1,9 @@
+javascript:
+(function(){
+
 if (mw.config.get('wgCanonicalNamespace') === "Special" && mw.config.get('wgCanonicalSpecialPageName') === "Block") {
 	function converttime() {
-		var timenode = document.getElementById("mw-input-wpExpiry-other");
+		var timenode = document.getElementsByName("wpExpiry-other")[0];
 		var time = timenode.value;
 		time = time.replace(/(\d+)年(\d+)月(\d+)日 \(.\) (\d+):(\d+)/, "$1/$2/$3 $4:$5");
 		time = time.replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/, "$1/$2/$3 $4:$5");
@@ -15,10 +18,12 @@ if (mw.config.get('wgCanonicalNamespace') === "Special" && mw.config.get('wgCano
 	node.id = "timeconvertbtn";
 	node.type = "button";
 	node.value = "轉換";
-	document.getElementById("mw-input-wpExpiry-other").parentNode.appendChild(node);
+	document.getElementsByName("wpExpiry-other")[0].parentNode.parentNode.appendChild(node);
 	timeconvertbtn.addEventListener("click", converttime);
 	function displayconvertbutton() {
-		timeconvertbtn.hidden = (document.getElementById("mw-input-wpExpiry").value !== "other");
+		timeconvertbtn.hidden = (document.getElementsByName("wpExpiry")[0].value !== "other");
 	}
-	document.getElementById("mw-input-wpExpiry").addEventListener("change", displayconvertbutton);
+	document.getElementsByName("wpExpiry")[0].addEventListener("change", displayconvertbutton);
 }
+
+})();
