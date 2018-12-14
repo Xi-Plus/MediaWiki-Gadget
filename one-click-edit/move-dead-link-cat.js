c2354ba06c912e1daf6c67700545d1345e217a02
xiplus <huangxuanyuxiplus@gmail.com>
2018-12-14 11:36:47 +0800

    Create move-dead-link-cat

---
+++ one-click-edit/move-dead-link-cat.js
@@ -0,0 +1,29 @@
+javascript: (function() {
+
+    var pagename = mw.config.get('wgPageName');
+    var m = pagename.match(/^Category:自(.+?)带有失效链接的条目$/);
+    if (m) {
+        var date = m[1];
+        var d = new Date(date);
+        date = d.getFullYear() + '年' + (d.getMonth() + 1) + '月';
+        var newpagename = mw.config.get('wgPageName').replace(/^(Category:自).+?(带有失效链接的条目)$/, '$1' + date + '$2');
+
+        new mw.Api().postWithEditToken({
+            action: 'move',
+            from: pagename,
+            to: newpagename,
+            movetalk: 1,
+            noredirect: 1,
+            reason: '移動到正確日期格式'
+        }).then(function(e) {
+            mw.notify('成功');
+            location.reload();
+        }, function(e) {
+            mw.notify('未知錯誤：' + e);
+        });
+    } else {
+        mw.notify('無法解析名稱');
+    }
+
+}
+)();
