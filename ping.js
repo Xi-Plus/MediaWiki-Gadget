2a1f92b7de55f67e359745394643df524cd4beb9
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-07 18:41:56 +0800

    ping: Update pattern and default template

--- ping.js
+++ ping.js
@@ -1,18 +1,18 @@
 javascript:
 (function(){
 
-var template = "ping";
+var template = "noping";
 var myusername = "Xiplus";
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
-var list = myPrefix.match(/\[\[(U|UT|User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
+var list = myPrefix.match(/\[\[(U|UT|User|User talk|User_talk|用戶|用户|使用者|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
 if (list === null) {
 	wpTextbox1.value = myPrefix + "{{"+template+"|}}" + mySuffix;
 	return;
 }
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
-	var temp = list[i].match(/\[\[(?:U|UT|User|User talk|User_talk|用戶|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
+	var temp = list[i].match(/\[\[(?:U|UT|User|User talk|User_talk|用戶|使用者|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
 	if (temp !== null && temp[1] != myusername) {
 		username = temp[1];
 		break;
