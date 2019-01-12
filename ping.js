d0b626dad73c2e09493d493b7d285f71a97462b8
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-12 20:43:53 +0800

    ping: Update pattern

--- ping.js
+++ ping.js
@@ -5,14 +5,14 @@ var template = "noping";
 var myusername = mw.config.get('wgUserName');
 var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
-var list = myPrefix.match(/\[\[(U|UT|User|User talk|User_talk|用戶|用户|使用者|用戶討論|用户讨论):[^|\]#\/]+[^\]]*\]\]/gi);
+var list = myPrefix.match(/\[\[((U|UT|User|User talk|User_talk|用戶|使用者|用户|用戶討論|用户讨论):|(Special|特殊):(Contributions|用户贡献|使用者貢獻|用戶貢獻)\/)[^|\]#\/]+[^\]]*\]\]/gi);
 if (list === null) {
 	wpTextbox1.value = myPrefix + "{{"+template+"|}}" + mySuffix;
 	return;
 }
 var username = "";
 for (var i = list.length - 1; i >= 0; i--) {
-	var temp = list[i].match(/\[\[(?:U|UT|User|User talk|User_talk|用戶|使用者|用户|用戶討論|用户讨论):([^|\]#\/]+)[^\]]*\]\]/i);
+	var temp = list[i].match(/\[\[(?:(?:U|UT|User|User talk|User_talk|用戶|使用者|用户|用戶討論|用户讨论):|(?:Special|特殊):(?:Contributions|用户贡献|使用者貢獻|用戶貢獻)\/)([^|\]#\/]+)[^\]]*\]\]/i);
 	if (temp !== null && temp[1][0].toUpperCase()+temp[1].substr(1) != myusername) {
 		username = temp[1];
 		break;
