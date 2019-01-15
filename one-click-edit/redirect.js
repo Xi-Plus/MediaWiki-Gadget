bfa2a815fc9c0c842eaaebc402b18ca902a5dc40
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-15 16:54:48 +0800

    Create redirect.js

---
+++ one-click-edit/redirect.js
@@ -0,0 +1,19 @@
+javascript: (function() {
+
+    new mw.Api().edit(mw.config.get('wgPageName'), function(revision) {
+        return {
+            text: "#REDIRECT [["+mw.config.get('wgPageName').substring(0, mw.config.get('wgPageName').length-1)+"]]",
+            summary: '[[Special:PermaLink/5553226|刪除請求]]'
+        };
+    }).then(function(e) {
+        mw.notify('成功');
+    }, function(e) {
+        if (e == 'nocreate-missing') {
+            mw.notify('頁面不存在');
+        } else {
+            mw.notify('未知錯誤：'+e);
+        }
+    });
+
+}
+)();
