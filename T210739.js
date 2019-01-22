c1f400a48f653b04727917f007d43984ae48eba5
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-22 21:39:14 +0800

    T210739: Add comment and msg variables

--- T210739.js
+++ T210739.js
@@ -1,35 +1,43 @@
+/**
+ * If you want to use this gadget as a user script, you need to load these modules: 'oojs-ui', 'mediawiki.api', 'mediawiki.notify'.
+ * It's just like https://en.wikipedia.org/wiki/User:JJMC89/T210739.js
+ *
+ * And localize the following message.
+ */
 (function() {
 
-mw.loader.using(['oojs-ui', 'mediawiki.api', 'mediawiki.notify'], function() {
+var msg_button_label = '使用API刪除目標頁';
+var msg_button_title = '使用API刪除目標頁';
+var msg_delete_reason = '[[WP:CSD#G8|G8]]: 删除以便移动';
+var msg_delete_success = '成功刪除 ';
+var msg_delete_fail = '刪除時發生錯誤：';
 
 if (document.getElementsByName("wpDeleteAndMove").length < 1) {
-	return;
+    return;
 }
 
 var button = new OO.ui.ButtonWidget( {
-	flags: [ 'primary', 'destructive' ],
-    label: '使用API刪除目標頁',
+    flags: [ 'primary', 'destructive' ],
+    label: msg_button_label,
     icon: 'trash',
-    title: '使用API刪除目標頁'
+    title: msg_button_title
 } ).on('click', function () {
-	var target = mw.config.get('wgFormattedNamespaces')[document.getElementsByName('wpNewTitleNs')[0].value];
-	if (target != '') {
-		target += ':';
-	}
-	target += document.getElementsByName('wpNewTitleMain')[0].value;
+    var target = mw.config.get('wgFormattedNamespaces')[document.getElementsByName('wpNewTitleNs')[0].value];
+    if (target !== '') {
+        target += ':';
+    }
+    target += document.getElementsByName('wpNewTitleMain')[0].value;
 
     new mw.Api().postWithEditToken({
         action: 'delete',
         title: target,
-        reason: '[[WP:CSD#G8|G8]]: 删除以便移动'
+        reason: msg_delete_reason
     }).then(function(e) {
-        mw.notify('成功刪除 ' +  target);
+        mw.notify(msg_delete_success +  target);
     }, function(e) {
-        mw.notify('刪除時發生錯誤：' + e);
+        mw.notify(msg_delete_fail + e);
     });
 });
 $(document.getElementsByName('wpMove')[0].parentElement.parentElement).append( button.$element );
-
-});
 
 })();
