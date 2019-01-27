a027bf8a15541d8d295338e22afa2c423a07e636
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-27 17:12:44 +0800

    translatewiki-fix-punctuation: Fix

--- translatewiki-fix-punctuation.js
+++ translatewiki-fix-punctuation.js
@@ -1,5 +1,5 @@
 javascript: (function() {
-    wpTextbox1.value = wpTextbox1.value.replace(/ ?\((.+?)\) ?/i, "（$1）")
+    wpTextbox1.value = wpTextbox1.value.replace(/ ?\((.+?)\) ?/i, "（$1）");
     wpTextbox1.value = wpTextbox1.value.replace(/ ?"(.+?)" ?/i, "「$1」");
     wpSummary.value = "修正標點符號";
 }
