af6ca4c65ff2dd915250a49747eb4d3142f47e09
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-07 20:27:24 +0800

    ep-done: Ask whether to sign

--- ep-done.js
+++ ep-done.js
@@ -1,12 +1,18 @@
 javascript: (function() {
     wpTextbox1.value = wpTextbox1.value.replace(/<!-- 在下一行填写你的理由\/在下一行填寫你的理由 -->\n?/, "").replace(/<!-- 以下文字请勿变更\/以下文字請勿變更 -->\n?/, "");
+    var repalcement = "\n{{$1";
     if (confirm("Done?")) {
-        wpTextbox1.value = wpTextbox1.value.replace(/\n*{{((?:Editprotected|EP)(?:\|.*?)?)}}\s*/i, "\n{{$1|ok=1|sign=--~~~~}}\n\n");
+        repalcement += "|ok=1";
         wpSummary.value = wpSummary.value + " EP done";
     } else {
-        wpTextbox1.value = wpTextbox1.value.replace(/\n*{{((?:Editprotected|EP)(?:\|.*?)?)}}\s*/i, "\n{{$1|no=1|sign=--~~~~}}\n\n");
+        repalcement += "|no=1";
         wpSummary.value = wpSummary.value + " EP not done";
     }
+    if (confirm("Sign?")) {
+        repalcement += "|sign=--~~~~";
+    }
+    repalcement += "}}\n\n";
+    wpTextbox1.value = wpTextbox1.value.replace(/\n*{{((?:Editprotected|EP)(?:\|.*?)?)}}\s*/i, repalcement);
     if (confirm("Save?")) {
         wpSave.click();
     }
