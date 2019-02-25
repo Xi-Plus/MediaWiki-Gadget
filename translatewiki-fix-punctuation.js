136f06057e1a2ad9213f35cd7071c56f6d35680f
xiplus <huangxuanyuxiplus@gmail.com>
2019-02-25 21:27:33 +0800

    translatewiki-fix-punctuation: Replace globally

--- translatewiki-fix-punctuation.js
+++ translatewiki-fix-punctuation.js
@@ -1,6 +1,6 @@
 javascript: (function() {
-    wpTextbox1.value = wpTextbox1.value.replace(/ ?\((.+?)\) ?/i, "（$1）");
-    wpTextbox1.value = wpTextbox1.value.replace(/ ?"(.+?)" ?/i, "「$1」");
+    wpTextbox1.value = wpTextbox1.value.replace(/ ?\((.+?)\) ?/gi, "（$1）");
+    wpTextbox1.value = wpTextbox1.value.replace(/ ?"(.+?)" ?/gi, "「$1」");
     wpSummary.value = "修正標點符號";
 }
 )();
