cb510eba83416399e820c1d61cd381496fd74778
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-27 13:31:17 +0800

    Create remove-hangon.js

---
+++ one-click-edit/remove-hangon.js
@@ -0,0 +1,22 @@
+javascript: (function() {
+
+    new mw.Api().edit(mw.config.get('wgPageName'), function(revision) {
+        var content = revision.content;
+        content.replace(/{{hang ?on\|.+?}}\n*/, "");
+        return {
+            text: content,
+            summary: '移除hangon'
+        };
+    }).then(function() {
+        mw.notify('成功');
+        location.reload();
+    }, function(e) {
+        if (e == 'nocreate-missing') {
+            mw.notify('頁面不存在');
+        } else {
+            mw.notify('未知錯誤：' + e);
+        }
+    });
+
+}
+)();
