e4cd65267b569ad804fa9805153b573fc5e13893
xiplus <huangxuanyuxiplus@gmail.com>
2019-06-08 15:39:38 +0800

    Fix quick-patrol

--- quick-patrol.js
+++ quick-patrol.js
@@ -1,7 +1,9 @@
 // <nowiki>
 (function() {
 
-    function patrol(id, revid) { // eslint-disable-line no-unused-vars
+    function patrol() { // eslint-disable-line no-unused-vars
+        let btnid = this.getAttribute('data-btnid');
+        let revid = this.getAttribute('data-revid');
         $.ajax({
             type: 'POST',
             url: location.protocol + mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/api.php',
@@ -9,24 +11,23 @@
                 'action': 'patrol',
                 'format': 'json',
                 'revid': revid,
-                'token': patroltoken
+                'token': mw.user.tokens.get('patrolToken')
             },
             success: function success(data) {
                 if (data.error !== undefined) {
-                    document.all["patrol_" + id].innerHTML = "API失敗：" + data.error.info;
-                    document.all["patrol_" + id].style.color = "#F00";
+                    document.all["patrol_" + btnid].innerHTML = "API失敗：" + data.error.info;
+                    document.all["patrol_" + btnid].style.color = "#F00";
                 } else {
-                    document.all["patrol_" + id].innerHTML = "已巡查";
-                    document.all["patrol_" + id].style["pointer-events"] = "none";
-                    document.all["patrol_" + id].style.color = "#888";
+                    document.all["patrol_" + btnid].innerHTML = "已巡查";
+                    document.all["patrol_" + btnid].style["pointer-events"] = "none";
+                    document.all["patrol_" + btnid].style.color = "#888";
                 }
             },
             error: function error() {
-                document.all["patrol_" + id].innerHTML = "Ajax失敗";
-                document.all["patrol_" + id].style.color = "#F00";
+                document.all["patrol_" + btnid].innerHTML = "Ajax失敗";
+                document.all["patrol_" + btnid].style.color = "#F00";
             }
         });
-
     }
 
     function partolall() { // eslint-disable-line no-unused-vars
@@ -40,32 +41,18 @@
     }
 
     if (mw.config.get('wgCanonicalSpecialPageName') === "Newpages") {
-        var patroltoken = "";
-        $.ajax({
-            type: 'GET',
-            url: location.protocol + mw.config.get('wgServer') + mw.config.get('wgScriptPath') + '/api.php',
-            data: {
-                'action': 'query',
-                'format': 'json',
-                'meta': 'tokens',
-                'type': 'patrol'
-            },
-            success: function success(data) {
-                patroltoken = data.query.tokens.patroltoken;
-            },
-            error: function error() {
-                alert("get token Error!");
-            }
-        });
         for (var i = 0; i < document.getElementsByClassName("not-patrolled").length; i++) {
-            document.getElementsByClassName("not-patrolled")[i].innerHTML += '<a id="patrol_' + i + '" onclick="patrol(' + i + ',' + document.getElementsByClassName("not-patrolled")[i].children[0].href.match(/oldid=(\d+)/)[1] + ')">巡查</a>';
+            let patrolbtn = $('<a id="patrol_' + i + '" data-btnid="' + i + '" data-revid="' + document.getElementsByClassName("not-patrolled")[i].children[0].href.match(/oldid=(\d+)/)[1] + '">巡查</a>');
+            patrolbtn.on('click', patrol);
+            patrolbtn.appendTo(document.getElementsByClassName("not-patrolled")[i]);
         }
         for (var i = 0; ; i++) {
             if (document.all["mw-content-text"].children[i] === undefined) {
                 break;
             } else if (document.all["mw-content-text"].children[i].tagName === "UL") {
-                document.all["mw-content-text"].children[i].innerHTML += '<li><a onclick="partolall();">巡查全部</a></li>';
-                break;
+                let patrolbtn = $('<li><a id="patrol_all">巡查全部</a></li>');
+                patrolbtn.on('click', partolall);
+                patrolbtn.appendTo(document.all["mw-content-text"].children[i]);
             }
         }
     }
