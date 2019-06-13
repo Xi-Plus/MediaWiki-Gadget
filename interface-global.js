c2d709efe2f43cc66c5bc0bc1f64054d09d333ed
xiplus <huangxuanyuxiplus@gmail.com>
2019-06-13 21:56:18 +0800

    Update interface-global.js

--- interface-global.js
+++ interface-global.js
@@ -25,7 +25,7 @@
 		// [[File:Krinkle_CVNSimpleOverlay_wiki.js]]
 		mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:Krinkle/Scripts/CVNSimpleOverlay_wiki.js&oldid=18174822&action=raw&ctype=text/javascript'); // 08:46, 5 August 2018 (UTC)
 		mw.loader.load("https://zh.wikipedia.org/w/index.php?title=User:Bluedeck/serve/edit-count.js&oldid=49623399&action=raw&ctype=text/javascript"); // 08:46, 5 August 2018 (UTC)
-
+		mw.loader.load('//en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-edittop.js&action=raw&ctype=text/javascript');
 
 		/* wikiplus */
 		// mw.loader.load('https://xiplus.ddns.net/Xiplus-zhWP/w.js');
@@ -54,13 +54,13 @@
 
 		/* FakeRollback */
 		if ($.inArray('sysop', mw.config.get('wgUserGroups')) === -1) {
-			mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:WhitePhosphorus/js/FakeRollback.js&oldid=18757250&action=raw&ctype=text/javascript'); // 11:25, 4 January 2019 (UTC)
+			mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:WhitePhosphorus/js/FakeRollback.js&action=raw&ctype=text/javascript');
 		}
 
 
 		/* AutoUndo */
 		if ($.inArray('sysop', mw.config.get('wgUserGroups')) === -1) {
-			mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:Xiplus/js/AutoUndo.js&action=raw&ctype=text/javascript'); // 08:47, 5 August 2018 (UTC)
+			mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:WhitePhosphorus/js/AutoUndo.js&action=raw&ctype=text/javascript');
 		}
 
 
@@ -83,6 +83,13 @@
 				mw.loader.load('https://meta.wikimedia.org/w/index.php?title=User:Xiplus/js/list-user-last-active.js&action=raw&ctype=text/javascript');
 				break;
 		}
+
+
+		/* smart rollback */
+		if(typeof(smartRollbackConfig) === 'undefined') smartRollbackConfig = {};
+		smartRollbackConfig.toolLinkMethod = 'p-tb';
+		smartRollbackConfig.editSummaries = ['批量回退無意義文字', '批量回退破壞'];
+		mw.loader.load('//meta.wikimedia.org/w/index.php?title=User:Hoo_man/smart_rollback.js&action=raw&ctype=text/javascript');
 
 
 		/* get-translatewiki-link */
