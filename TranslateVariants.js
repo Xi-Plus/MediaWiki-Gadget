5b26992ed196507c668ca69d352e3ea355886b5c
xiplus <huangxuanyuxiplus@gmail.com>
2019-08-29 14:08:47 +0800

    Synchronize

--- TranslateVariants.js
+++ TranslateVariants.js
@@ -50,6 +50,15 @@
 		} else {
 			var zhwpapi = new mw.ForeignApi('//zh.wikipedia.org/w/api.php');
 		}
+		var basepagetext = '';
+		let table = $('<div id="TranslateVariants">').prependTo('#bodyContent');
+		$('<div style="color:red">提醒：TranslateVariants小工具仍在試驗階段，編輯完成後亦請複查真正做出的編輯是否正確！</div>').appendTo(table);
+		var langqueue = [];
+
+		langs.forEach(lang => {
+			langqueue.push(lang);
+		});
+
 		api.get({
 			action: 'query',
 			prop: 'revisions',
@@ -87,101 +96,100 @@
 					.replace('&#125;-', '}-')
 					.replace(/&#58;/g, ':')
 			});
-			let p = [];
-			langs.forEach(lang => {
-				p.push(
-					zhwpapi.parse(
-						'{{NoteTA|G1=IT|G2=MediaWiki}}<div id="TVcontent">' + text + '</div>',
-						{
-							'uselang': lang,
-							'prop': 'text'
-						}
-					).then(function(data) {
-						result[lang] = $('<div/>').html(data).find('#TVcontent').text();
-					}, function(err) {
-						mw.notify('解析' + lang + '時發生錯誤：' + err);
-					})
-				);
-			})
-			return Promise.all(p);
-		}).then(function() {
-			let table = $('<div id="TranslateVariants">').prependTo('#bodyContent');
-			$('<div style="color:red">提醒：TranslateVariants小工具仍在試驗階段，編輯完成後亦請複查真正做出的編輯是否正確！</div>').appendTo(table);
+			basepagetext = text;
+
+			process();
+		});
+
+		function process() {
+			if (langqueue.length === 0) {
+				return;
+			}
+
+			var lang = langqueue.shift();
+			var diffTable = $('<div id="TranslateVariants-diff-' + lang + '">').appendTo(table);
+			$('<hr>').appendTo(table);
+
 			const basename = mw.config.get('wgPageName').replace(/\/zh$/, '');
-			let editlangs = [];
-			langs.forEach(lang => {
-				if (lang == 'zh') {
-					editlangs.push(['', lang]);
-				} else {
-					editlangs.push(['/' + lang, lang]);
+			var targetTitle;
+			if (lang == 'zh') {
+				targetTitle = basename + '';
+			} else {
+				targetTitle = basename + '/' + lang;
+			}
+
+			var newtext;
+
+			zhwpapi.parse(
+				'{{NoteTA|G1=IT|G2=MediaWiki}}<div id="TVcontent">' + basepagetext + '</div>',
+				{
+					'uselang': lang,
+					'prop': 'text'
 				}
-			});
-			let diffTables = {}
-			editlangs.forEach(lang => {
-				diffTables[lang[1]] = $('<div id="TranslateVariants-diff-' + lang[1] + '">').appendTo(table);
-				$('<hr>').appendTo(table);
-			});
-			editlangs.forEach(lang => {
-				let newtext = result[lang[1]];
-				let targetTitle = basename + lang[0];
-				api.post({
+			).then(function(data) {
+				newtext = $('<div/>').html(data).find('#TVcontent').text();
+
+				return api.post({
 					action: 'query',
 					prop: 'revisions',
 					titles: [targetTitle],
 					rvdifftotext: newtext,
 					formatversion: '2',
-				}).then(function(data) {
-					let diffTable = diffTables[lang[1]];
-					let tool = $('<div><a href="' + mw.util.getUrl(targetTitle) + '">' + lang[1] + '（' + langname[lang[1]] + '）</a>（<a href="' + mw.util.getUrl(targetTitle, { action: 'edit' }) + '">編</a>）</div>').appendTo(diffTable);
-					let page = data.query.pages[0];
-					if (page.missing) {
-						let submit = $('<button style="float: right;">發佈頁面</button>').appendTo(tool);
-						submit.on('click', function() {
-							this.remove();
-							api.create(
-								targetTitle,
-								{ summary: TranslateVariants.summary.replace(/\$1/g, mw.config.get('wgPageName')) },
-								newtext
-							).then(function() {
-								mw.notify('已編輯 ' + targetTitle);
-							}, function(e) {
-								mw.notify('編輯 ' + targetTitle + ' 發生錯誤：' + e);
-							});
-						});
-						$('<pre>').html(newtext.replace(/[<>&]/gim, function(s) {
-							return "&#" + s.charCodeAt(0) + ";";
-						})).appendTo(diffTable);
-						return;
-					}
-					let diff = page.revisions[0].diff.body;
-					if (diff == '') {
-						$('<span style="float: right;">無變更</span>').appendTo(tool);
-					} else {
-						let submit = $('<button style="float: right;">發佈變更</button>').appendTo(tool);
-						submit.on('click', function() {
-							this.remove();
-							api.edit(
-								targetTitle,
-								function() {
-									return {
-										text: newtext,
-										summary: TranslateVariants.summary.replace(/\$1/g, mw.config.get('wgPageName')),
-										nocreate: false
-									};
-								}
-							).then(function() {
-								mw.notify('已編輯 ' + targetTitle);
-							}, function(e) {
-								mw.notify('編輯 ' + targetTitle + ' 發生錯誤：' + e);
-							});
-						});
-						$('<table class="diff">').html(diff).prepend('<colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup>').appendTo(diffTable);
-					}
-				}, function(err) {
-					mw.notify('取得' + lang + '差異時發生錯誤：' + err);
 				});
+			}, function(err) {
+				mw.notify('解析' + lang + '時發生錯誤：' + err);
+			}).then(function(data) {
+				let tool = $('<div><a href="' + mw.util.getUrl(targetTitle) + '">' + lang + '（' + langname[lang] + '）</a>（<a href="' + mw.util.getUrl(targetTitle, { action: 'edit' }) + '">編</a>）</div>').appendTo(diffTable);
+				let page = data.query.pages[0];
+				if (page.missing) {
+					let submit = $('<button style="float: right;">發佈頁面</button>').appendTo(tool);
+					submit.on('click', function() {
+						this.remove();
+						api.create(
+							targetTitle,
+							{ summary: TranslateVariants.summary.replace(/\$1/g, mw.config.get('wgPageName')) },
+							newtext
+						).then(function() {
+							mw.notify('已編輯 ' + targetTitle);
+						}, function(e) {
+							mw.notify('編輯 ' + targetTitle + ' 發生錯誤：' + e);
+						});
+					});
+					$('<pre>').html(newtext.replace(/[<>&]/gim, function(s) {
+						return "&#" + s.charCodeAt(0) + ";";
+					})).appendTo(diffTable);
+					return;
+				}
+				let diff = page.revisions[0].diff.body;
+				if (diff == '') {
+					$('<span style="float: right;">無變更</span>').appendTo(tool);
+				} else {
+					let submit = $('<button style="float: right;">發佈變更</button>').appendTo(tool);
+					submit.on('click', function() {
+						this.remove();
+						api.edit(
+							targetTitle,
+							function() {
+								return {
+									text: newtext,
+									summary: TranslateVariants.summary.replace(/\$1/g, mw.config.get('wgPageName')),
+									nocreate: false
+								};
+							}
+						).then(function() {
+							mw.notify('已編輯 ' + targetTitle);
+						}, function(e) {
+							mw.notify('編輯 ' + targetTitle + ' 發生錯誤：' + e);
+						});
+					});
+					$('<table class="diff">').html(diff).prepend('<colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup>').appendTo(diffTable);
+				}
+			}, function(err) {
+				mw.notify('取得' + lang + '差異時發生錯誤：' + err);
+			}).always(function() {
+				process();
 			});
-		});
+		}
 	}
 
 })();
