eb3649d5d175aa1c675249fe678d795a2dd83415
xiplus <huangxuanyuxiplus@gmail.com>
2019-10-11 20:23:05 +0800

    Check claims exists

--- my-ACG/add-acg-gamer-url.js
+++ my-ACG/add-acg-gamer-url.js
@@ -1,21 +1,36 @@
 javascript: (function() {
 
-    var url = prompt('URL');
-    if (!url) {
-        mw.notify('已取消');
-        return;
-    }
-
-    new mw.Api().postWithEditToken({
-        'action': 'wbcreateclaim',
+    var api = new mw.Api();
+    api.get({
+        'action': 'wbgetclaims',
         'format': 'json',
         'entity': mw.config.get('wgTitle'),
-        'snaktype': 'value',
-        'property': 'P1',
-        'value': '"' + url + '"',
-    }).then(function() {
-        mw.notify('成功新增');
-        location.reload();
+        'property': 'P1'
+    }).then(function(data) {
+        if (data['claims']['P1'] !== undefined) {
+            mw.notify('已存在資料');
+            return;
+        }
+
+        var url = prompt('URL');
+        if (!url) {
+            mw.notify('已取消');
+            return;
+        }
+
+        api.postWithEditToken({
+            'action': 'wbcreateclaim',
+            'format': 'json',
+            'entity': mw.config.get('wgTitle'),
+            'snaktype': 'value',
+            'property': 'P1',
+            'value': '"' + url + '"',
+        }).then(function() {
+            mw.notify('成功新增');
+            location.reload();
+        }, function(e) {
+            mw.notify('未知錯誤：' + e);
+        });
     }, function(e) {
         mw.notify('未知錯誤：' + e);
     });
