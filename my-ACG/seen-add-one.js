c25a987f4528dcbd1e4a5b5b0147f60d4ae1b5bb
xiplus <huangxuanyuxiplus@gmail.com>
2019-10-11 19:54:02 +0800

    Create seen-add-one.js

---
+++ my-ACG/seen-add-one.js
@@ -0,0 +1,50 @@
+(function() {
+
+    function seenAddOne() {
+        var api = new mw.Api();
+        api.get({
+            'action': 'wbgetclaims',
+            'format': 'json',
+            'entity': mw.config.get('wgTitle'),
+            'property': 'P28'
+        }).then(function(data) {
+            var guid = data['claims']['P28'][0]['id'];
+            var oldvalue = parseInt(data['claims']['P28'][0]['mainsnak']['datavalue']['value']['amount']);
+            var newvalue = oldvalue + 1;
+
+            if (!confirm('已看集數從 ' + oldvalue + ' 改為 ' + newvalue)) {
+                mw.notify('已取消');
+                return;
+            }
+
+            api.postWithEditToken({
+                'action': 'wbsetclaimvalue',
+                'format': 'json',
+                'claim': guid,
+                'value': JSON.stringify({
+                    'amount': '+' + newvalue,
+                    'unit': '1'
+                }),
+                'snaktype': 'value',
+            }).then(function() {
+                mw.notify('成功更新');
+                location.reload();
+            }, function(e) {
+                mw.notify('未知錯誤：' + e);
+            });
+
+        }, function(e) {
+            mw.notify('未知錯誤：' + e);
+        });
+
+    }
+
+    if ($('div#P28 div.wikibase-snakview-value').text() != $('div#P27 div.wikibase-snakview-value').text()) {
+        $('<button style="margin-left: 10px;">+1</button>')
+            .on('click', seenAddOne)
+            .appendTo($('div#P28 div.wikibase-snakview-body'));
+
+    }
+
+}
+)();
