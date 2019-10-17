ca6178cf8c3fedfa45a52cfb6d1132617453a293
xiplus <huangxuanyuxiplus@gmail.com>
2019-10-17 19:48:10 +0800

    Update hide-usertalk-in-ep-cat.js

--- hide-usertalk-in-ep-cat.js
+++ hide-usertalk-in-ep-cat.js
@@ -1,6 +1,10 @@
 javascript:
 (function() {
 
+	if (mw.config.get('wgPageName') !== 'Category:維基百科編輯被保護頁面請求') {
+		return;
+	}
+
 	$('.mw-category li>a').each(function(i, e) {
 		if (e.href.match(/wiki\/User_talk:[^/]+$/)) {
 			$(e).parent().hide();
