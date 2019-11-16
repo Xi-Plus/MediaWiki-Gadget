ae918396b2aaefa36eccdc0169bf4c878aeecf25
xiplus <huangxuanyuxiplus@gmail.com>
2019-11-16 22:17:04 +0800

    Use mw.Api

--- Unsigned.js
+++ Unsigned.js
@@ -1,11 +1,5 @@
 javascript:
-/* globals APIedit */
-/* 本工具需配合 https://github.com/Xi-Plus/Xiplus-zhWP/blob/master/APIedit.js 使用 */
 (function() {
-
-	function edit(content) {
-		return content + "\n==您忘了簽名==\n{{subst:Uw-tilde|" + mw.config.get('wgPageName') + "}}--~~~~";
-	}
 
 	var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
 	var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
@@ -14,11 +8,54 @@ javascript:
 		wpTextbox1.value = myPrefix + "{{subst:unsigned|" + username + "}}" + mySuffix;
 		wpSummary.value = "[[Template:Unsigned|補簽名]] [[User:" + username + "|" + username + "]]（[[User talk:" + username + "|對話]]｜[[Special:Contributions/" + username + "|貢獻]]）";
 		wpMinoredit.click();
+
 		var finish = function() {
-			if (confirm("Save?")) wpSave.click();
+			if (confirm("Save?")) {
+				wpSave.click();
+			}
 		};
-		if (confirm("Uw-tilde?")) APIedit("User_talk:" + username, "單層級通知：沒有在討論頁上簽名，於[[" + mw.config.get('wgPageName') + "]]", edit, false, finish);
-		else finish();
+
+		if (confirm("Uw-tilde?")) {
+			var api = new mw.Api();
+			var talkmessage = '==您忘了簽名==\n{{subst:Uw-tilde|' + mw.config.get('wgPageName') + '}}--~~~~';
+			var talksummary = '單層級通知：沒有在討論頁上簽名，於[[' + mw.config.get('wgPageName') + ']]';
+			var talkpage = "User_talk:" + username;
+
+			api.edit(
+				talkpage,
+				function(revision) {
+					return {
+						text: revision.content + '\n\n' + talkmessage,
+						summary: talksummary,
+						nocreate: false
+					};
+				}
+			).then(function() {
+				mw.notify('已通知');
+				finish();
+			}, function(e) {
+				if (e == 'nocreate-missing') {
+					api.create(
+						talkpage,
+						{
+							summary: talksummary
+						},
+						talkmessage
+					).then(function() {
+						mw.notify('已通知');
+						finish();
+					}, function(e) {
+						mw.notify('建立討論頁發生錯誤：' + e);
+						finish();
+					});
+				} else {
+					mw.notify('編輯討論頁發生錯誤：' + e);
+					finish();
+				}
+			});
+		} else {
+			finish();
+		}
 	}
 
 })();
