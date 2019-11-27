31c84678af21cadc14cdb9bd01ef599a8669962e
xiplus <huangxuanyuxiplus@gmail.com>
2019-11-27 11:41:15 +0800

    Create quick-link-wikidata-szywp.js

---
+++ one-click-edit/quick-link-wikidata-szywp.js
@@ -0,0 +1,126 @@
+javascript:
+(function() {
+	if ($.inArray(mw.config.get('wgNamespaceNumber'), [0, 10, 14]) === -1) {
+		/* Enabled on main, template, category namespaces only. */
+		return;
+	}
+
+	if ($('#t-wikibase').length > 0) {
+		/* Disabled for linked pages */
+		return;
+	}
+
+	function LinkData(localurl, sitelang, sitecode) {
+		var pagename = prompt('輸入頁面名稱', window.getSelection().toString());
+		if (pagename === null) {
+			mw.notify('動作已取消');
+			return;
+		}
+
+		/* 取得對應條目的名字 */
+		var localapi = new mw.ForeignApi(localurl);
+		localapi.get({
+			action: 'query',
+			format: 'json',
+			prop: 'pageprops',
+			titles: pagename,
+			redirects: 'yes',
+			converttitles: 'yes',
+		}).done(function(data) {
+			var hasitem = false;
+			var hastitle = false;
+			var title;
+			var dataid;
+			if (data.query && data.query.pages) {
+				for (var pageid in data.query.pages) {
+					var item = data.query.pages[pageid];
+					if (item.missing === '') {
+						continue;
+					}
+					hastitle = true;
+					title = item.title;
+					if (item.pageprops && item.pageprops.wikibase_item) {
+						hasitem = true;
+						dataid = item.pageprops.wikibase_item;
+					}
+				}
+			}
+			if (!hastitle) {
+				mw.notify('找不到對應條目，可能是因為尚未建立或者名稱不一致。');
+			} else {
+				/* 加入連結 */
+				var api = new mw.ForeignApi('https://www.wikidata.org/w/api.php');
+				api.get({
+					action: 'query',
+					meta: 'tokens'
+				}).done(function(data) {
+					if (!hasitem) {
+						if (!confirm('對應項目沒有維基數據。是否添加？')) {
+							mw.notify('已取消建立項目。');
+							return;
+						}
+						var targetsite = sitecode;
+						api.post({
+							action: 'wbeditentity',
+							'new': 'item',
+							token: data.query.tokens.csrftoken,
+							'data': JSON.stringify({
+								labels: [{
+									language: sitelang,
+									value: title,
+								}, {
+									language: 'szy',
+									value: mw.config.get('wgPageName'),
+								}],
+								sitelinks: [{
+									site: targetsite,
+									title: title,
+								}, {
+									site: mw.config.get('wgDBname'),
+									title: mw.config.get('wgPageName'),
+								}],
+							})
+						}).done(function() {
+							mw.notify('成功建立新的維基數據項目。');
+						}).fail(function() {
+							mw.notify('存取維基數據時發生錯誤。');
+						});
+					} else {
+						api.post({
+							action: 'wbsetsitelink',
+							id: dataid,
+							token: data.query.tokens.csrftoken,
+							linksite: mw.config.get('wgDBname'),
+							linktitle: mw.config.get('wgPageName'),
+						}).done(function(data) {
+							if (data.success) {
+								mw.notify('成功連結至現有維基數據項目。');
+							} else {
+								mw.notify('存取維基數據時發生錯誤。');
+							}
+						}).fail(function() {
+							mw.notify('存取維基數據時發生錯誤。');
+						});
+					}
+				}).fail(function() {
+					mw.notify('存取維基數據時發生錯誤。');
+				});
+			}
+		}, function() {
+			mw.notify('取得條目名稱時發生錯誤。');
+		})
+	}
+
+	var wikis = [
+		{ text: '連結到中維', url: 'https://zh.wikipedia.org/w/api.php', lang: 'zh', sitecode: 'zhwiki' },
+		{ text: '英維', url: 'https://en.wikipedia.org/w/api.php', lang: 'en', sitecode: 'enwiki' },
+	];
+	wikis.forEach(wiki => {
+		var link = mw.util.addPortletLink('p-namespaces', '#', wiki.text);
+		$(link).on('click', function() {
+			LinkData(wiki.url, wiki.lang, wiki.sitecode);
+		})
+	});
+
+}
+)();
