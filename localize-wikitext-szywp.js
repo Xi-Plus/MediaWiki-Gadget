2ab35cd74db443f69a0a1dd1f0e36d19f9c37879
xiplus <huangxuanyuxiplus@gmail.com>
2019-11-28 19:50:03 +0800

    Create localize-wikitext-szywp.js

---
+++ localize-wikitext-szywp.js
@@ -0,0 +1,51 @@
+(function() {
+
+	if (mw.config.get('wgAction') !== 'edit') {
+		return;
+	}
+
+	function main() {
+
+		var zhwpapi = new mw.ForeignApi('//zh.wikipedia.org/w/api.php');
+
+		let text = wpTextbox1.value;
+
+		text = text.replace(/[[\]{}<>|:*'_#&\s]/gim, function(s) {
+			return "&#" + s.charCodeAt(0) + ";";
+		});
+		text = text.replace(/(&#91;&#91;)((?:(?!&#124;)(?!&#93;).)+?)(&#124;(?:(?!&#93;).)+?&#93;&#93;)/g, '$1-{$2}-$3');
+		text = text.replace(/-&#123;(.+?)&#125;-/g, function(s) {
+			return s
+				.replace('-&#123;', '-{')
+				.replace('&#125;-', '}-')
+				.replace(/&#124;/g, '|')
+				.replace(/&#32;/g, ' ')
+				.replace(/&#61;/g, '=')
+				.replace(/&#62;/g, '>')
+				.replace(/&#58;/g, ':')
+		});
+
+		zhwpapi.parse(
+			'{{NoteTA|G1=IT|G2=MediaWiki}}<div id="TVcontent">' + text + '</div>',
+			{
+				'uselang': 'zh-tw',
+				'prop': 'text'
+			}
+		).then(function(data) {
+			var newtext = $('<div/>').html(data).find('#TVcontent').text();
+
+			wpTextbox1.value = newtext;
+			wpSummary.value = '本地化';
+			wpMinoredit.checked = true;
+			wpDiff.click();
+
+		}, function(err) {
+			mw.notify('解析時發生錯誤：' + err);
+		});
+
+	}
+
+	var link = mw.util.addPortletLink('p-namespaces', '#', '本地化');
+	$(link).on('click', main);
+
+})();
