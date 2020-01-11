cd63c7c1c3ca77bbadb0a77c8ce598a3085d01ee
xiplus <huangxuanyuxiplus@gmail.com>
2020-01-11 09:28:12 +0800

    Update block-user-for-sock.js

--- one-click-edit/block-user-for-sock.js
+++ one-click-edit/block-user-for-sock.js
@@ -57,7 +57,7 @@ javascript: (function() {
 )();
 
 
-// 1 week, notalk
+// infinite, notalk
 javascript: (function() {
     var user = mw.config.get('wgRelevantUserName');
     if (user === null) {
@@ -82,5 +82,36 @@ javascript: (function() {
     }, function(e) {
         mw.notify('未知錯誤：' + e);
     });
+}
+)();
+
+
+// infinite, notalk, noemail
+javascript: (function() {
+
+    var user = mw.config.get('wgRelevantUserName');
+    if (user === null) {
+        mw.notify('未能找到對應用戶');
+        return;
+    }
+    var reason = '確認為傀儡';
+    var expiry = 'infinite';
+    if (!confirm('封鎖 ' + user + ' ？\n理由：' + reason + '\n期限：' + expiry)) {
+        return;
+    }
+    new mw.Api().postWithEditToken({
+        action: 'block',
+        user: user,
+        expiry: expiry,
+        reason: reason,
+        nocreate: 1,
+        autoblock: 1,
+        noemail: 1,
+    }).then(function() {
+        mw.notify('成功封鎖');
+    }, function(e) {
+        mw.notify('未知錯誤：' + e);
+    });
+
 }
 )();
