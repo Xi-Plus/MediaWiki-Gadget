12ec63bf9353c4b970cab51bedc03b5a088b4501
xiplus <huangxuanyuxiplus@gmail.com>
2020-07-22 14:18:24 +0800

    Ask whether to use short diff link

--- getdiff.js
+++ getdiff.js
@@ -3,10 +3,10 @@ javascript:
 
 	var link = "";
 	if (mw.config.get("wgDiffNewId")) {
-		if ($(".diff-multi").length > 0) {
-			link = "Special:Diff/" + mw.config.get("wgDiffOldId") + "/" + mw.config.get("wgDiffNewId");
-		} else {
+		if (!$(".diff-multi").length && confirm('Short version?')) {
 			link = "Special:Diff/" + mw.config.get("wgDiffNewId");
+		} else {
+			link = "Special:Diff/" + mw.config.get("wgDiffOldId") + "/" + mw.config.get("wgDiffNewId");
 		}
 	} else if (mw.config.get("wgRevisionId")) {
 		link = "Special:PermaLink/" + mw.config.get("wgRevisionId");
