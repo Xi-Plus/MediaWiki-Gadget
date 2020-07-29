a59f3a88c949d1fbec436c6964eb9edd6ae37362
xiplus <huangxuanyuxiplus@gmail.com>
2020-07-29 13:40:52 +0800

    Delay to update links

--- enable-discussion-tools.js
+++ enable-discussion-tools.js
@@ -2,30 +2,34 @@
 // <nowiki>
 (function() {
 
-    $('a').each(function(i, e) {
-        var shorthref = e.getAttribute('href') || '';
-        if (shorthref.match(/^(|#)$/)) {
-            return;
-        }
-        try {
-            var url = new URL(e.href);
-            if (url.host.match(/\.(wikipedia|wiktionary|wikiquote|wikisource|wikinews|wikivoyage|wikibooks|wikiversity|wikimedia|mediawiki|wikidata)\.org$/)) {
-                url.searchParams.set('dtenable', '1');
-                e.href = url.href;
-            }
-        } catch (err) {
-            console.error(e, err);
-        }
-    });
+	var updateLinks = function() {
+		$('a').each(function(i, e) {
+			var shorthref = e.getAttribute('href') || '';
+			if (shorthref.match(/^(|#)$/)) {
+				return;
+			}
+			try {
+				var url = new URL(e.href);
+				if (url.host.match(/\.(wikipedia|wiktionary|wikiquote|wikisource|wikinews|wikivoyage|wikibooks|wikiversity|wikimedia|mediawiki|wikidata)\.org$/)) {
+					url.searchParams.set('dtenable', '1');
+					e.href = url.href;
+				}
+			} catch (err) {
+				console.error(e, err);
+			}
+		});
+	};
 
-    var url = new URL(window.location.href);
-    url.searchParams.set('dtenable', '1');
-    mw.util.addPortletLink(
+	setTimeout(updateLinks, 1000);
+
+	var url = new URL(window.location.href);
+	url.searchParams.set('dtenable', '1');
+	mw.util.addPortletLink(
 		'p-tb',
 		url.href,
 		'Enable DiscussionTools',
 		't-dtenable'
-    );
+	);
 
 }
 )();
