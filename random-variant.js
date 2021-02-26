372e7d9750395d9f4cd6d16547956c3f535d606e
xiplus <huangxuanyuxiplus@gmail.com>
2021-02-26 10:25:28 +0800

    Create random-variant.js

---
+++ random-variant.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+// <nowiki>
+(function() {
+	var variantList = ['zh-cn', 'zh-tw', 'zh-hk', 'zh-mo', 'zh-sg', 'zh-my'];
+	var pickVariant = function() {
+		return variantList[Math.floor(Math.random() * variantList.length)];
+	};
+
+	var updateLinks = function() {
+		$('a').each(function(i, e) {
+			var shorthref = e.getAttribute('href') || '';
+			if (shorthref.match(/^(|#)$/)) {
+				return;
+			}
+			try {
+				var url = new URL(e.href);
+				if (url.host.match(/zh\.wikipedia\.org$/)) {
+					url.searchParams.set('variant', pickVariant());
+					e.href = url.href;
+				}
+			} catch (err) {
+				console.error(e, err);
+			}
+		});
+	};
+
+	setTimeout(updateLinks, 1000);
+}
+)();
+// </nowiki>
