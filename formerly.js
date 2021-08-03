03c733f0d923fd7b30e59590b98602d4ea0507b7
xiplus <huangxuanyuxiplus@gmail.com>
2021-08-03 11:48:32 +0800

    Update formerly.js

--- formerly.js
+++ formerly.js
@@ -1,18 +1,19 @@
 javascript:
 (function() {
 
-	var newtitle = prompt("title");
-	if (newtitle === null) return;
-	var m;
-	if ((m = wpTextbox1.value.match(/<s>\[\[(.+?)]]<\/s>→\[\[(.+?)]]/)) !== null) {
-		var oldtitle = m[1];
-		newtitle = m[2];
-	} else {
-		var oldtitle = wpTextbox1.value.match(/== *(.+?) *==/)[1];
+	var newtitle = prompt('title');
+	if (newtitle === null) {
+		return;
 	}
-	wpTextbox1.value = wpTextbox1.value.replace(/== *(.+?) *== *\n?/, "==[[" + newtitle + "]]==\n{{formerly|[[" + oldtitle + "]]}}\n");
-	wpSummary.value = "/* " + newtitle + " */ 替換標題[[" + oldtitle + "]]→[[" + newtitle + "]]";
+	var oldtitle = wpTextbox1.value.match(/==+ *(.+?) *==+/)[1];
+	wpTextbox1.value = wpTextbox1.value.replace(
+		new RegExp('(==+) *' + mw.util.escapeRegExp(oldtitle) + ' *(==+)')
+		, '$1 ' + newtitle + ' $2\n{{formerly|' + oldtitle + '}}'
+	);
+	wpSummary.value = '/* ' + newtitle + ' */ 更改標題，原標題為：' + oldtitle;
 	wpMinoredit.click();
-	if (confirm("Save?")) wpSave.click();
+	if (confirm('Save?')) {
+		wpSave.click();
+	}
 
 })();
