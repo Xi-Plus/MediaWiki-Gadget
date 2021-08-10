b94e3369d67251eab78a04b0dc6fdad679286843
xiplus <huangxuanyuxiplus@gmail.com>
2021-08-10 10:57:18 +0800

    Lint

--- one-click-edit/o4-to-category-redirect.js
+++ one-click-edit/o4-to-category-redirect.js
@@ -1,7 +1,7 @@
 javascript: (function() {
 	new mw.Api().edit(mw.config.get('wgPageName'), function(revision) {
 		return {
-			text: revision.content.replace(/[\s\S]*#(?:重定向|REDIRECT) *\[\[\:?(?:Category|Cat|分类|分類):(.+?)\]\][\s\S]*/i, "{{分类重定向|$1}}"),
+			text: revision.content.replace(/[\s\S]*#(?:重定向|REDIRECT) *\[\[:?(?:Category|Cat|分类|分類):(.+?)\]\][\s\S]*/i, "{{分类重定向|$1}}"),
 			summary: '改成分類重定向',
 		};
 	}).then(function() {
