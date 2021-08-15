968d358fe1ace029226d242f72cfa414f00d701d
xiplus <huangxuanyuxiplus@gmail.com>
2021-08-15 12:02:32 +0800

    Add config and load automatically

--- get-translatewiki-link.js
+++ get-translatewiki-link.js
@@ -1,6 +1,14 @@
 (function() {
 
-	var lang = "zh-hant";
+	if (typeof window.GetTranslatewikiLink == 'undefined') {
+		window.GetTranslatewikiLink = {};
+	}
+
+	var GetTranslatewikiLink = window.GetTranslatewikiLink;
+
+	if (typeof (GetTranslatewikiLink.lang) != 'string') {
+		GetTranslatewikiLink.lang = 'zh-hant';
+	}
 
 	function getTranslatewikiLink(str) {
 		var match = str.match(/\(([^:()]+)/g);
@@ -8,13 +16,15 @@
 		if (match !== undefined) {
 			$(match).each(function(i, m) {
 				m = m.substr(1);
-				res.push('<a href="https://translatewiki.net/wiki/MediaWiki:' + m + '/' + lang + '" target="_blank">(' + m + ')</a><sup><a href="' + mw.config.get("wgServer") + mw.config.get("wgArticlePath").replace('$1', 'MediaWiki:' + m) + '" target="_blank">local</a></sup>');
+				res.push('<a href="https://translatewiki.net/wiki/MediaWiki:' + m + '/' + GetTranslatewikiLink.lang + '" target="_blank">(' + m + ')</a><sup><a href="' + mw.config.get("wgServer") + mw.config.get("wgArticlePath").replace('$1', 'MediaWiki:' + m) + '" target="_blank">local</a></sup>');
 			});
 		}
 		return res;
 	}
 
 	function main() {
+		mw.notify('Click messages key to get link to translatewiki.')
+
 		$(document).click(function(event) {
 			var el = $(event.target);
 			if ($(el).attr("class") == "mw-notification-content") {
@@ -71,13 +81,22 @@
 
 	mw.loader.using(['mediawiki.util']).done(function() {
 		if (mw.util.getParamValue('uselang') === 'qqx') {
-			var node = mw.util.addPortletLink('p-cactions', '#', '取得translatewiki連結');
-			$(node).click(function(e) {
-				e.preventDefault();
-				$(node).hide();
-
+			if (mw.util.getParamValue('linktlw')) {
 				main();
-			});
+			} else {
+				var node = mw.util.addPortletLink('p-cactions', '#', 'Links to translatewiki');
+				$(node).click(function(e) {
+					e.preventDefault();
+					$(node).hide();
+
+					main();
+				});
+			}
+		} else {
+			var url = new URL(window.location.href);
+			url.searchParams.set('uselang', 'qqx');
+			url.searchParams.set('linktlw', '1');
+			mw.util.addPortletLink('p-cactions', url.href, 'Links to translatewiki');
 		}
 	});
 
