06ed4c910c6ce67c88e157259cb662810cdace3e
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-14 21:26:33 +0800

    Update getdiff.js

--- getdiff.js
+++ getdiff.js
@@ -1,17 +1,19 @@
 javascript:
 (function() {
 
-	var link = "";
-	if (mw.config.get("wgDiffNewId")) {
-		if (!$(".diff-multi").length && confirm('Short version?')) {
-			link = "Special:Diff/" + mw.config.get("wgDiffNewId");
+	var link = '';
+	if (mw.config.get('wgDiffNewId')) {
+		if (!$('.diff-multi').length && confirm('Short version?')) {
+			link = 'Special:Diff/' + mw.config.get('wgDiffNewId');
 		} else {
-			link = "Special:Diff/" + mw.config.get("wgDiffOldId") + "/" + mw.config.get("wgDiffNewId");
+			link = 'Special:Diff/' + mw.config.get('wgDiffOldId') + '/' + mw.config.get('wgDiffNewId');
 		}
-	} else if (mw.config.get("wgRevisionId")) {
-		link = "Special:PermaLink/" + mw.config.get("wgRevisionId");
+	} else if (mw.config.get('wgRevisionId')) {
+		link = 'Special:PermaLink/' + mw.config.get('wgRevisionId');
+	} else if (mw.config.get('wgCanonicalSpecialPageName') == 'Log' && mw.util.getParamValue('logid')) {
+		link = 'Special:Redirect/logid/' + mw.util.getParamValue('logid')
 	} else {
-		mw.notify("Cannot get link");
+		mw.notify('Cannot get link');
 		return;
 	}
 
@@ -20,6 +22,6 @@ javascript:
 	hash = hash.replace(/}/g, '&#x7D;');
 	link += hash;
 
-	prompt("wikitext", "[[" + link + "]]");
+	prompt('wikitext', '[[' + link + ']]');
 
 })();
