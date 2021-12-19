a9e1595ec9b6b5bf9fd2e2c1e19daa980ab847bd
xiplus <huangxuanyuxiplus@gmail.com>
2021-12-19 15:23:41 +0800

    Update Unsigned-last.js

--- Unsigned-last.js
+++ Unsigned-last.js
@@ -1,54 +1,53 @@
 javascript:
-/* globals APIedit */
+/* globals mw */
 /* 本工具需配合 https://github.com/Xi-Plus/MediaWiki-Gadget/blob/master/APIedit.js 使用 */
 (function() {
 
-	var username = "";
-	$.ajax({
-		type: "GET",
-		url: mw.config.get("wgServer") + mw.config.get("wgScriptPath") + "/api.php",
-		data: {
-			action: "query",
-			format: "json",
-			prop: "revisions",
-			titles: mw.config.get('wgPageName'),
-			rvprop: "user",
-			rvlimit: "1",
-		},
-		success: function success(data) {
-			for (var id in data.query.pages) {
-				username = data.query.pages[id].revisions[0].user;
-				break;
-			}
-			if (username === null || username === "") {
-				alert("讀取編者錯誤");
-			} else if (confirm("補簽名: " + username)) {
-				next();
-			}
-		},
-		error: function error() {
-			alert("抓取最後編者錯誤");
-		},
-	});
+	var api = new mw.Api();
+	api.get({
+		'action': 'query',
+		'format': 'json',
+		'prop': 'revisions',
+		'titles': mw.config.get('wgPageName'),
+		'rvprop': 'timestamp|user',
+		'rvlimit': '1',
+		'utf8': 1,
+		'formatversion': '2',
+	}).then(data => {
+		var username = data.query.pages[0].revisions[0].user;
+		var timestamp = data.query.pages[0].revisions[0].timestamp;
 
-	function unsignedit(content) {
-		return content + "{{subst:unsigned|" + username + "}}";
-	}
-
-	function noticeedit(content) {
-		return content + "\n==您忘了簽名==\n{{subst:Uw-tilde|" + mw.config.get('wgPageName') + "}}--~~~~";
-	}
-
-	function finish() {
-		APIedit(mw.config.get('wgPageName'), "[[Template:Unsigned|補簽名]] [[User:" + username + "|" + username + "]]（[[User talk:" + username + "|對話]]｜[[Special:Contributions/" + username + "|貢獻]]）", unsignedit, true);
-	}
-
-	function next() {
-		if (confirm("Uw-tilde?")) {
-			APIedit("User_talk:" + username, "單層級通知：沒有在討論頁上簽名，於[[" + mw.config.get('wgPageName') + "]]", noticeedit, false, finish);
-		} else {
-			finish();
+		if (!confirm('補簽名: ' + username)) {
+			return $.Deferred().reject('cancel');
 		}
-	}
+
+		api.edit(mw.config.get('wgPageName'), function(revision) {
+			return {
+				text: revision.content + '{{subst:Unsigned|' + username + '|' + timestamp + '}}',
+				summary: '[[Template:Unsigned|補簽名]] [[User:' + username + '|' + username + ']]（[[User talk:' + username + '|對話]]｜[[Special:Contributions/' + username + '|貢獻]]）',
+				minor: true,
+			};
+		}).then(function() {
+			mw.notify('已補簽名');
+		});
+
+		if (confirm('Uw-tilde?')) {
+			api.newSection(
+				'User talk:' + username,
+				'通知：沒有在討論頁上簽名',
+				'{{subst:Uw-tilde|' + mw.config.get('wgPageName') + '}}--~~~~'
+			).then(_data => {
+				mw.notify('已通知使用者');
+			}).catch(_error => {
+				mw.notify('通知使用者發生錯誤', { type: 'error' });
+			})
+		}
+	}).catch(error => {
+		if (error === 'cancel') {
+			mw.notify('已取消', { type: 'error' });
+		} else {
+			mw.notify('抓取最後編者發生錯誤', { type: 'error' });
+		}
+	});
 
 })();
