b9414faa065ba8594ff409535f6b518cd64bf72e
xiplus <huangxuanyuxiplus@gmail.com>
2022-01-27 18:14:58 +0800

    Use CategoryMembersIndicator and create categorymembers-add-time.js

---
+++ categorymembers-add-time.js
@@ -0,0 +1,57 @@
+javascript:
+(function() {
+	var CategorymembersAddTime = window.CategorymembersAddTime;
+	if (typeof CategorymembersAddTime === 'undefined') {
+		CategorymembersAddTime = {};
+	}
+	if (typeof CategorymembersAddTime.titles !== 'object') {
+		CategorymembersAddTime.titles = [
+			'Category:快速删除候选',
+			'Category:封禁及禁制申诉',
+		];
+	}
+
+	var pageName = mw.config.get('wgPageName');
+	if (CategorymembersAddTime.titles.indexOf(pageName) === -1) {
+		return;
+	}
+
+	function main() {
+		var addText = window.CategoryMembersIndicator.addText;
+
+		var api = new mw.Api();
+		var now = new Date();
+		var usedTitle = [];
+
+		api.get({
+			'action': 'query',
+			'list': 'recentchanges',
+			'rcprop': 'timestamp|comment',
+			'rclimit': 'max',
+			'rctype': 'categorize',
+			'rctitle': pageName,
+		}).done(data => {
+			data.query.recentchanges.forEach(row => {
+				var m = row.comment.match(/\[\[:([^\]]+)\]\]/);
+				if (m) {
+					var title = m[1].replace(/ /g, '_');
+					if (usedTitle.indexOf(title) === -1 && row.comment.indexOf('已添加至分类') > -1) {
+						var current = new Date(row.timestamp);
+						var period = (now.getTime() - current.getTime()) / 1000 / 3600;
+						if (period > 1) {
+							addText(title, 'period', Math.round(period) + ' hr');
+						} else {
+							addText(title, 'period', Math.round(period * 60) + ' min')
+						}
+					}
+					usedTitle.push(title);
+				}
+			});
+		});
+	}
+
+	mw.loader.getScript('https://meta.wikimedia.org/w/index.php?title=User:Xiplus/js/category-members-indicator.js&action=raw&ctype=text/javascript').then(function() {
+		main();
+	});
+
+})();
--- csd-reason-in-csd-cat.js
+++ csd-reason-in-csd-cat.js
@@ -1,87 +1,64 @@
 javascript:
 (function() {
 
-	if (mw.config.get('wgPageName') !== 'Category:快速删除候选') return;
-
-	var api = new mw.Api();
-	var now = new Date();
-
-	var titlelist = [];
-	$('.mw-category-generated a').each(function(i, e) {
-		var title = decodeURIComponent(e.href.replace(/^.*?\/wiki\/(.+?)(?:\?.+)?$/, '$1'));
-		$('<span>').attr('id', 'cricc-item-' + title).insertAfter(e);
-		titlelist.push(title);
-	});
-	var addText = function(title, type, text) {
-		$('<span>').text('（' + text + '）').addClass('cricc-' + type).appendTo(document.getElementById('cricc-item-' + title));
+	if (mw.config.get('wgPageName') !== 'Category:快速删除候选') {
+		return;
 	}
 
-	for (var i = 0; i < titlelist.length; i += 50) {
-		var titlestr = titlelist.slice(i, i + 50).join('|');
-		api.get({
-			'action': 'query',
-			'format': 'json',
-			'prop': 'revisions',
-			'titles': titlestr,
-			'rvprop': 'content',
-		}).done(data => {
-			for (i in data.query.pages) {
-				var page = data.query.pages[i];
-				if (page.missing === undefined) {
-					var title = page.title.replace(/ /g, '_');
-					var m = page.revisions[0]['*'].match(/{{\s*(?:d|delete|csd|速删|速刪)\s*\|\s*(.+?)\s*}}/i);
-					if (m !== null) {
-						var m2 = m[1].match(/bot=Jimmy-bot\|([^|}]+)/);
-						if (m2 !== null) {
-							addText(title, 'reason', m2[1]);
-						} else {
-							addText(title, 'reason', m[1]);
+	function main() {
+		var api = new mw.Api();
+		var addText = window.CategoryMembersIndicator.addText;
+
+		var titlelist = [];
+		$('.mw-category-generated a').each(function(i, e) {
+			var title = decodeURIComponent(e.href.replace(/^.*?\/wiki\/(.+?)(?:\?.+)?$/, '$1'));
+			titlelist.push(title);
+		});
+
+		for (var i = 0; i < titlelist.length; i += 50) {
+			var titlestr = titlelist.slice(i, i + 50).join('|');
+			api.get({
+				'action': 'query',
+				'format': 'json',
+				'prop': 'revisions',
+				'titles': titlestr,
+				'rvprop': 'content',
+			}).done(data => {
+				for (i in data.query.pages) {
+					var page = data.query.pages[i];
+					if (page.missing === undefined) {
+						var title = page.title.replace(/ /g, '_');
+						var m = page.revisions[0]['*'].match(/{{\s*(?:d|delete|csd|速删|速刪)\s*\|\s*(.+?)\s*}}/i);
+						if (m !== null) {
+							var m2 = m[1].match(/bot=Jimmy-bot\|([^|}]+)/);
+							if (m2 !== null) {
+								addText(title, 'cricc-reason', m2[1]);
+							} else {
+								addText(title, 'cricc-reason', m[1]);
+							}
+						}
+						if (page.revisions[0]['*'].match(/{{\s*Notmandarin\s*\|/i)) {
+							addText(title, 'cricc-reason', 'G14');
+						}
+						if (page.revisions[0]['*'].match(/{{\s*(Nowcommons|Now[ _]+Commons|Ncd|F7)\s*\|/i)) {
+							addText(title, 'cricc-reason', 'F7');
+						}
+						if (page.revisions[0]['*'].match(/{{\s*AFC submission\s*\|/i)) {
+							addText(title, 'cricc-reason', 'AFC');
+						}
+						if ((m = page.revisions[0]['*'].match(/{{\s*(?:hang ?on|有爭議|有争议)\s*\|\s*(.+?)\s*}}/i)) !== null) {
+							addText(title, 'cricc-hangon', 'Hangon：' + m[1]);
 						}
 					}
-					if (page.revisions[0]['*'].match(/{{\s*Notmandarin\s*\|/i)) {
-						addText(title, 'reason', 'G14');
-					}
-					if (page.revisions[0]['*'].match(/{{\s*(Nowcommons|Now[ _]+Commons|Ncd|F7)\s*\|/i)) {
-						addText(title, 'reason', 'F7');
-					}
-					if (page.revisions[0]['*'].match(/{{\s*AFC submission\s*\|/i)) {
-						addText(title, 'reason', 'AFC');
-					}
-					if ((m = page.revisions[0]['*'].match(/{{\s*(?:hang ?on|有爭議|有争议)\s*\|\s*(.+?)\s*}}/i)) !== null) {
-						addText(title, 'hangon', 'Hangon：' + m[1]);
-					}
 				}
-			}
-		}).fail(() => {
-			alert('Error!');
-		})
+			}).fail(() => {
+				alert('Error!');
+			})
+		}
 	}
 
-	var usedTitle = [];
-	api.get({
-		'action': 'query',
-		'list': 'recentchanges',
-		'rcprop': 'timestamp|comment',
-		'rclimit': 'max',
-		'rctype': 'categorize',
-		'rctitle': 'Category:快速删除候选',
-	}).done(data => {
-		data.query.recentchanges.forEach(row => {
-			var m = row.comment.match(/\[\[:([^\]]+)\]\]/);
-			if (m) {
-				var title = m[1].replace(/ /g, '_');
-				if (usedTitle.indexOf(title) === -1 && row.comment.indexOf('已添加至分类') > -1) {
-					var current = new Date(row.timestamp);
-					var period = (now.getTime() - current.getTime()) / 1000 / 3600;
-					if (period > 1) {
-						addText(title, 'period', Math.round(period) + ' hr');
-					} else {
-						addText(title, 'period', Math.round(period * 60) + ' min')
-					}
-				}
-				usedTitle.push(title);
-			}
-		});
-	})
+	mw.loader.getScript('https://meta.wikimedia.org/w/index.php?title=User:Xiplus/js/category-members-indicator.js&action=raw&ctype=text/javascript').then(function() {
+		main();
+	});
 
 })();
