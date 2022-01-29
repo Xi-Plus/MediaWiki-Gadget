dde279844769af180b1c686e38037801361365c7
xiplus <huangxuanyuxiplus@gmail.com>
2022-01-29 21:53:06 +0800

    Update saveto.js

--- saveto.js
+++ saveto.js
@@ -1,13 +1,13 @@
 javascript:
 (function() {
 
-    var page = prompt("page");
-    if (page == "" || page == null) return;
-    page = page.replace(/_/g, " ");
-    var pages = page.split("|");
-    wpTextbox1.value = wpTextbox1.value.replace(/(==.+==) *\n\n?/, "$1\n{{saveto|" + page + "}}\n");
-    wpSummary.value = wpSummary.value + " saveto [[" + pages.join("]]、[[") + "]]";
+    var page = prompt('page');
+    if (page == '' || page == null) return;
+    page = page.replace(/_/g, ' ');
+    var pages = page.split('|');
+    wpTextbox1.value = wpTextbox1.value.replace(/(==.+==) *\n\n?/, '$1\n{{存檔至|' + page + '}}\n');
+    wpSummary.value = wpSummary.value + ' 存檔至[[' + pages.join(']]、[[') + ']]';
     wpMinoredit.click();
-    if (confirm("Save?")) wpSave.click();
+    if (confirm('Save?')) wpSave.click();
 
 })();
