bd47214c638e477e77a1ade0f1eacea6a1c90112
xiplus <huangxuanyuxiplus@gmail.com>
2022-03-03 19:40:02 +0800

    Improve Unsigned.js

--- Unsigned.js
+++ Unsigned.js
@@ -1,69 +1,132 @@
-javascript:
-(function() {
+javascript: (async function () {
+	var api = new mw.Api();
+	var textbox = document.getElementById('wpTextbox1');
 
-	var myPrefix = wpTextbox1.value.substring(0, wpTextbox1.selectionStart);
-	var mySuffix = wpTextbox1.value.substring(wpTextbox1.selectionEnd);
-	var username = prompt("Username");
-	if (username !== "" && username !== null) {
+	var myPrefix = textbox.value.substring(0, textbox.selectionStart);
+	var selectedText = textbox.value.substring(textbox.selectionStart, textbox.selectionEnd);
+	var mySuffix = textbox.value.substring(textbox.selectionEnd);
+
+	var username = '',
+		timestamp = '';
+	if (selectedText) {
+		await api
+			.get({
+				action: 'query',
+				format: 'json',
+				prop: 'revisions',
+				titles: mw.config.get('wgPageName'),
+				utf8: 1,
+				formatversion: '2',
+				rvprop: 'timestamp|user|content',
+				rvslots: 'main',
+				rvlimit: '50',
+			})
+			.done(function (res) {
+				var found = false;
+				for (const revision of res.query.pages[0].revisions) {
+					console.log(revision.user, revision.timestamp);
+					if (revision.slots.main.content.includes(selectedText)) {
+						found = true;
+						username = revision.user;
+						timestamp = revision.timestamp;
+					} else {
+						if (found) {
+							break;
+						}
+					}
+				}
+				if (found) {
+					mw.notify('Added by ' + username + ' at ' + timestamp);
+				} else {
+					mw.notify('Author not found', { type: 'error' });
+				}
+			});
+	}
+
+	if (!username) {
+		username = prompt('Username');
+	}
+	if (username !== '' && username !== null) {
 		username = username.trim();
-		wpTextbox1.value = myPrefix + "{{subst:unsigned|" + username + "}}" + mySuffix;
+		textbox.value =
+			myPrefix +
+			selectedText +
+			'{{subst:unsigned|' +
+			username +
+			'|' +
+			new Morebits.date(timestamp).format('YYYY年M月D日 (ddd) HH:mm', 'utc') +
+			' (UTC)}}' +
+			mySuffix;
 		var summary = '[[Template:Unsigned|補簽名]]';
 		if (mw.util.isIPAddress(username)) {
-			summary += '[[User:' + username + '|' + username + ']]（[[User talk:' + username + '|對話]]｜[[Special:Contributions/' + username + '|貢獻]]）';
+			summary +=
+				'[[User:' +
+				username +
+				'|' +
+				username +
+				']]（[[User talk:' +
+				username +
+				'|對話]]｜[[Special:Contributions/' +
+				username +
+				'|貢獻]]）';
 		} else {
 			summary += '[[Special:Contributions/' + username + '|' + username + ']]（[[User talk:' + username + '|留言]]）';
 		}
-		wpSummary.value = summary;
+		document.getElementById('wpSummary').value = summary;
+		document.getElementById('wpMinoredit').click();
 
-		wpMinoredit.click();
-
-		var finish = function() {
-			if (confirm("Save?")) {
-				wpSave.click();
+		var finish = function () {
+			if (confirm('Save?')) {
+				document.getElementById('wpSave').click();
 			}
 		};
 
-		if (confirm("Uw-tilde?")) {
-			var api = new mw.Api();
-			var talkmessage = '==您忘了簽名==\n{{subst:Uw-tilde|' + mw.config.get('wgPageName') + '}}--~~~~';
+		if (confirm('Notify ' + username + '?')) {
+			var talkmessage = '== 您忘了簽名 ==\n{{subst:Uw-tilde|' + mw.config.get('wgPageName') + '}}--~~~~';
 			var talksummary = '單層級通知：沒有在討論頁上簽名，於[[' + mw.config.get('wgPageName') + ']]';
-			var talkpage = "User_talk:" + username;
+			var talkpage = 'User_talk:' + username;
 
-			api.edit(
-				talkpage,
-				function(revision) {
+			api
+				.edit(talkpage, function (revision) {
 					return {
 						text: revision.content + '\n\n' + talkmessage,
 						summary: talksummary,
 						nocreate: false,
 					};
-				}
-			).then(function() {
-				mw.notify('已通知');
-				finish();
-			}, function(e) {
-				if (e == 'nocreate-missing') {
-					api.create(
-						talkpage,
-						{
-							summary: talksummary,
-						},
-						talkmessage
-					).then(function() {
+				})
+				.then(
+					function () {
 						mw.notify('已通知');
 						finish();
-					}, function(e) {
-						mw.notify('建立討論頁發生錯誤：' + e);
-						finish();
-					});
-				} else {
-					mw.notify('編輯討論頁發生錯誤：' + e);
-					finish();
-				}
-			});
+					},
+					function (e) {
+						if (e == 'nocreate-missing') {
+							api
+								.create(
+									talkpage,
+									{
+										summary: talksummary,
+									},
+									talkmessage
+								)
+								.then(
+									function () {
+										mw.notify('已通知');
+										finish();
+									},
+									function (e) {
+										mw.notify('建立討論頁發生錯誤：' + e);
+										finish();
+									}
+								);
+						} else {
+							mw.notify('編輯討論頁發生錯誤：' + e);
+							finish();
+						}
+					}
+				);
 		} else {
 			finish();
 		}
 	}
-
 })();
