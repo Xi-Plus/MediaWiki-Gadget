065c716640363ee46965517321a058eff9d16eb7
xiplus <huangxuanyuxiplus@gmail.com>
2023-06-18 22:08:21 +0800

    Create Special-Contributions-IP.js

---
+++ get-list/Special-Contributions-IP.js
@@ -0,0 +1,14 @@
+javascript: (function() {
+
+    var text = '';
+    $('.mw-anonuserlink').each(function(i, e) {
+        if (text.indexOf(e.innerText) === -1) {
+            text += e.innerText + '<br>';
+        }
+    });
+
+    var win = window.open('');
+    win.document.body.innerHTML = text;
+
+}
+)();
